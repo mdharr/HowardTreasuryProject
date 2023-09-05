2994ad322b125de9d3df6e66b1f596220c8a10c3
Michael Harrington <mdharr7@gmail.com>
2023-09-05 15:29:15 +0800

    Add PasswordEncoder to spring boot application in application.java file with bean method annotation.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/HowardTreasuryApplication.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/HowardTreasuryApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.howardtreasury;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HowardTreasuryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HowardTreasuryApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
 	}
 
 }
