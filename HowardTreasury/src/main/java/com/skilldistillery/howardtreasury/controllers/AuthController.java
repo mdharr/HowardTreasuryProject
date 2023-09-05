b70b7b70a823d48c1d25037ff2268e1bb2387725
Michael Harrington <mdharr7@gmail.com>
2023-09-05 18:36:37 +0800

    Add jdbc url, username, and password properties to application.properties file in boot project.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/AuthController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/AuthController.java
@@ -1,9 +1,17 @@
 package com.skilldistillery.howardtreasury.controllers;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.skilldistillery.howardtreasury.entities.User;
 import com.skilldistillery.howardtreasury.services.AuthService;
 
 @Controller
@@ -11,4 +19,24 @@ import com.skilldistillery.howardtreasury.services.AuthService;
 public class AuthController {
   @Autowired
   private AuthService authService;
+  
+	@PostMapping("register")
+	public User register(@RequestBody User user, HttpServletResponse res) {
+	  if (user == null) {
+	     res.setStatus(400);
+	     return null;
+	  }
+	  user = authService.register(user);
+	  return user;
+	}
+	 
+	@GetMapping("authenticate")
+	public User authenticate(Principal principal, HttpServletResponse res) {
+	  if (principal == null) { // no Authorization header sent
+	     res.setStatus(401);
+	     res.setHeader("WWW-Authenticate", "Basic");
+	     return null;
+	  }
+	  return authService.getUserByUsername(principal.getName());
+	}
 }
