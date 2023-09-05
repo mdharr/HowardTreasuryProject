495fa773416f6e597398192faa75f20083ecf474
Michael Harrington <mdharr7@gmail.com>
2023-09-05 19:42:44 +0800

    Change environment import from development to environment

--- ngHowardTreasury/src/app/services/auth.service.ts
+++ ngHowardTreasury/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, catchError, throwError, tap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 
 @Injectable({
