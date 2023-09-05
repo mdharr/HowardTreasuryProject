22935790a3a80e5a6245dff1f9bd915fb6826541
Michael Harrington <mdharr7@gmail.com>
2023-09-05 18:47:01 +0800

    Full stack auth test.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/AuthController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/AuthController.java
@@ -5,16 +5,16 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.howardtreasury.entities.User;
 import com.skilldistillery.howardtreasury.services.AuthService;
 
-@Controller
+@RestController
 @CrossOrigin({"*", "http://localhost"})
 public class AuthController {
   @Autowired
