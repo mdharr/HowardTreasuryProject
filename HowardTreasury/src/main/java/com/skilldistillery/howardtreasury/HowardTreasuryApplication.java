e87b926797b03d90f88927650c2c92a30972b9a7
Michael Harrington <mdharr7@gmail.com>
2023-09-05 15:23:32 +0800

    Stub Boot/REST project

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/HowardTreasuryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.howardtreasury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowardTreasuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowardTreasuryApplication.class, args);
+	}
+
+}
---
+++ HowardTreasury/src/test/java/com/skilldistillery/howardtreasury/HowardTreasuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.howardtreasury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowardTreasuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
