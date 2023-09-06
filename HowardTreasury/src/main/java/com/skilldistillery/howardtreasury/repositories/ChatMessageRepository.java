18ba445a4c5a09632c6bd88b01474fe3cbbab2e5
Michael Harrington <mdharr7@gmail.com>
2023-09-06 21:11:02 +0800

    Create necessary method in chat message repository and methods to perform crud on chat messages in chat message service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/ChatMessageRepository.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/ChatMessageRepository.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.howardtreasury.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.howardtreasury.entities.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Integer> {
 
+	List<ChatMessage> findByChatRoomIdOrderByCreatedAt(int chatRoomId);
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/ChatMessageServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/ChatMessageServiceImpl.java
@@ -1,13 +1,62 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.ChatMessage;
+import com.skilldistillery.howardtreasury.entities.ChatRoom;
+import com.skilldistillery.howardtreasury.entities.User;
 import com.skilldistillery.howardtreasury.repositories.ChatMessageRepository;
+import com.skilldistillery.howardtreasury.repositories.ChatRoomRepository;
+import com.skilldistillery.howardtreasury.repositories.UserRepository;
 
 @Service
 public class ChatMessageServiceImpl {
 
 	@Autowired
-	private ChatMessageRepository chatMessageRepo;
+    private ChatMessageRepository chatMessageRepo;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepo;
+
+    // Send a message to a chat room
+    public ChatMessage sendMessageToChatRoom(int chatRoomId, int senderUserId, String messageContent) {
+        User sender = userRepo.findById(senderUserId)
+                .orElseThrow(() -> new IllegalArgumentException("Sender not found"));
+
+        ChatRoom chatRoom = chatRoomRepo.findById(chatRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("Chat room not found"));
+
+        ChatMessage message = new ChatMessage();
+        message.setMessageContent(messageContent);
+        message.setUser(sender);
+        message.setChatRoom(chatRoom);
+
+        return chatMessageRepo.save(message);
+    }
+
+    // Get messages for a chat room
+    public List<ChatMessage> getChatRoomMessages(int chatRoomId) {
+        return chatMessageRepo.findByChatRoomIdOrderByCreatedAt(chatRoomId);
+    }
+
+    // Delete a message
+    public void deleteMessage(int messageId) {
+        chatMessageRepo.deleteById(messageId);
+    }
+
+    // Edit a message
+    public ChatMessage editMessage(int messageId, String newMessageContent) {
+        ChatMessage message = chatMessageRepo.findById(messageId)
+                .orElseThrow(() -> new IllegalArgumentException("Message not found"));
+
+        message.setMessageContent(newMessageContent);
+
+        return chatMessageRepo.save(message);
+    }
 }
