1ea39c720454466230e82baaee1e0b3dc8a6c0d6
Michael Harrington <mdharr7@gmail.com>
2023-09-06 16:11:23 +0800

    Create and stub out junit test case for ChatMessage. Entity mapping test successful.

--- JPAHowardTreasury/src/test/java/com/skilldistillery/howardtreasury/entities/ChatMessageTest.java
+++ JPAHowardTreasury/src/test/java/com/skilldistillery/howardtreasury/entities/ChatMessageTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.howardtreasury.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,25 +14,36 @@ import org.junit.jupiter.api.Test;
 
 class ChatMessageTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private ChatMessage chatMessage;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAHowardTreasury");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		chatMessage = em.find(ChatMessage.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		chatMessage = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_entity_mapping() {
+		assertNotNull(chatMessage);
+		assertEquals("Welcome to the Robert E. Howard Treasury chat room!", chatMessage.getMessageContent());
 	}
 
 }
