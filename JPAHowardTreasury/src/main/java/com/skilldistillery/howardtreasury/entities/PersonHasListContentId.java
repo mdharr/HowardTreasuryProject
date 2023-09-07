bd47711938b2065ef50238bd2926e953489550b1
Michael Harrington <mdharr7@gmail.com>
2023-09-07 13:58:42 +0800

    Stub out person has list content and embeddable person has list content id.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/PersonHasListContentId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/PersonHasListContentId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class PersonHasListContentId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "person_has_list_content")
+public class PersonHasListContentId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+    @Column(name = "person_id")
+    private int personId;
+    
+    @Column(name = "list_content_id")
+    private int listContentId;
+
+	public PersonHasListContentId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonHasListContentId(int personId, int listContentId) {
+		super();
+		this.personId = personId;
+		this.listContentId = listContentId;
+	}
+
+	public int getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+
+	public int getListContentId() {
+		return listContentId;
+	}
+
+	public void setListContentId(int listContentId) {
+		this.listContentId = listContentId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(listContentId, personId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonHasListContentId other = (PersonHasListContentId) obj;
+		return listContentId == other.listContentId && personId == other.personId;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonHasListContentId [personId=" + personId + ", listContentId=" + listContentId + "]";
+	}
+    
 }
