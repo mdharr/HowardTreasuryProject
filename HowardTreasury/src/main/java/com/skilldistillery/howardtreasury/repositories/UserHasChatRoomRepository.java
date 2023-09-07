a7d4ef9f1892993444584dcfeb9fa6b88c6dcd5f
Michael Harrington <mdharr7@gmail.com>
2023-09-07 10:50:45 +0800

    Icebox live chat feature for now.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/UserHasChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.howardtreasury.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skilldistillery.howardtreasury.entities.UserHasChatRoom;
-
-public interface UserHasChatRoomRepository extends JpaRepository<UserHasChatRoom, Integer> {
-
-}
