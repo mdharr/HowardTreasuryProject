9870fdee1a9ec41a7c75d032940212a7d4ea8893
Michael Harrington <mdharr7@gmail.com>
2023-09-07 13:52:22 +0800

    Stub out person entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Person.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Person.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.List;
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Person {
@@ -11,5 +17,73 @@ public class Person {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String title;
+	
+    @ManyToMany
+    @JoinTable(
+        name = "person_has_list_content",
+        joinColumns = @JoinColumn(name = "person_id"),
+        inverseJoinColumns = @JoinColumn(name = "list_content_id")
+    )
+    private List<ListContent> listContents;
+
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(int id, String title, List<ListContent> listContents) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.listContents = listContents;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<ListContent> getListContents() {
+		return listContents;
+	}
+
+	public void setListContents(List<ListContent> listContents) {
+		this.listContents = listContents;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", title=" + title + ", listContents=" + listContents + "]";
+	}
 
 }
