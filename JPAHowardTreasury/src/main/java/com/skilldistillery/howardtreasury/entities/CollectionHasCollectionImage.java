7bfa829a1228530a13f683524d94b67d6bf2b9c6
Michael Harrington <mdharr7@gmail.com>
2023-09-08 16:38:14 +0800

    Stub out embeddable id for collection has collection image entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasCollectionImage.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasCollectionImage.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "collection_has_collection_image")
 public class CollectionHasCollectionImage {
 
+	@EmbeddedId
+	private CollectionHasCollectionImageId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "collection_id")
+	@MapsId("collectionId")
+	private Collection collection;
+	
+    @ManyToOne
+    @JoinColumn(name = "collection_image_id")
+    @MapsId("collectionImageId")
+    private CollectionImage collectionImage;
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasCollectionImageId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasCollectionImageId.java
@@ -1,5 +1,76 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class CollectionHasCollectionImageId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "collection_has_collection_image")
+public class CollectionHasCollectionImageId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "collection_id")
+	private int collectionId;
+	
+    @Column(name = "collection_image_id")
+    private int collectionImageId;
+
+	public CollectionHasCollectionImageId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CollectionHasCollectionImageId(int collectionId, int collectionImageId) {
+		super();
+		this.collectionId = collectionId;
+		this.collectionImageId = collectionImageId;
+	}
+
+	public int getCollectionId() {
+		return collectionId;
+	}
+
+	public void setCollectionId(int collectionId) {
+		this.collectionId = collectionId;
+	}
+
+	public int getCollectionImageId() {
+		return collectionImageId;
+	}
+
+	public void setCollectionImageId(int collectionImageId) {
+		this.collectionImageId = collectionImageId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(collectionId, collectionImageId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionHasCollectionImageId other = (CollectionHasCollectionImageId) obj;
+		return collectionId == other.collectionId && collectionImageId == other.collectionImageId;
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionHasCollectionImageId [collectionId=" + collectionId + ", collectionImageId="
+				+ collectionImageId + "]";
+	}
+    
 }
