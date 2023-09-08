e7e3f0d84521b7bbb0f917c9617fe73b235d5a6e
Michael Harrington <mdharr7@gmail.com>
2023-09-08 15:09:53 +0800

    Stub out mappings in collection entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Collection.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Collection.java
@@ -1,5 +1,36 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Collection {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "series_id")
+	private Series series;
+	
+    @ManyToMany(mappedBy = "collections")
+    private List<Story> stories;
+    
+    @ManyToMany(mappedBy = "collections")
+    private List<Poem> poems;
+    
+    @ManyToMany(mappedBy = "collections")
+    private List<Person> persons;
+    
+    @ManyToMany(mappedBy = "collections")
+    private List<Miscellanea> miscellaneas;
 
 }
