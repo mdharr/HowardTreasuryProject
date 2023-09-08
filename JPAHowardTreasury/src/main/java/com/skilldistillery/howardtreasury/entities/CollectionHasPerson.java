0ff9facdc156f1c0711beb9b8d7109931d0f6bb0
Michael Harrington <mdharr7@gmail.com>
2023-09-08 15:41:46 +0800

    Stub out collection has person entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPerson.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPerson.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -12,7 +14,7 @@ import javax.persistence.Table;
 public class CollectionHasPerson {
 
 	@EmbeddedId
-	private CollectionHasStoryId id;
+	private CollectionHasPersonId id;
 	
 	@ManyToOne
 	@JoinColumn(name = "collection_id")
@@ -23,4 +25,63 @@ public class CollectionHasPerson {
     @JoinColumn(name = "person_id")
     @MapsId("personId")
     private Person person;
+
+	public CollectionHasPerson() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CollectionHasPerson(CollectionHasPersonId id, Collection collection, Person person) {
+		super();
+		this.id = id;
+		this.collection = collection;
+		this.person = person;
+	}
+
+	public CollectionHasPersonId getId() {
+		return id;
+	}
+
+	public void setId(CollectionHasPersonId id) {
+		this.id = id;
+	}
+
+	public Collection getCollection() {
+		return collection;
+	}
+
+	public void setCollection(Collection collection) {
+		this.collection = collection;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionHasPerson other = (CollectionHasPerson) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionHasPerson [id=" + id + ", collection=" + collection + ", person=" + person + "]";
+	}
+    
 }
