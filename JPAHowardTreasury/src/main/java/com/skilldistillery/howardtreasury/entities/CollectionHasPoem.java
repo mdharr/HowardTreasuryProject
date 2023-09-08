39445ceed7c81da4a1f606dd67116427bbe1052a
Michael Harrington <mdharr7@gmail.com>
2023-09-08 15:43:00 +0800

    Progress on collection has poem entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPoem.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPoem.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "collection_has_poem")
 public class CollectionHasPoem {
 
+	@EmbeddedId
+	private CollectionHasPoemId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "collection_id")
+	@MapsId("collectionId")
+	private Collection collection;
+	
+    @ManyToOne
+    @JoinColumn(name = "poem_id")
+    @MapsId("poemId")
+    private Poem poem;
 }
