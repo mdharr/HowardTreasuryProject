01812672e6f4a66ecf93628881c9f9a6d3ded452
Michael Harrington <mdharr7@gmail.com>
2023-09-08 16:31:43 +0800

    Stub out collection image entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionImage.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionImage.java
@@ -1,5 +1,93 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "collection_image")
 public class CollectionImage {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+	
+    @ManyToMany
+    @JoinTable(
+    		name = "collection_has_collection_image",
+    		joinColumns = @JoinColumn(name = "collection_image_id"),
+    		inverseJoinColumns = @JoinColumn(name = "collection_id")
+    		)
+    private List<Collection> collections;
+
+	public CollectionImage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CollectionImage(int id, String imageUrl, List<Collection> collections) {
+		super();
+		this.id = id;
+		this.imageUrl = imageUrl;
+		this.collections = collections;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Collection> getCollections() {
+		return collections;
+	}
+
+	public void setCollections(List<Collection> collections) {
+		this.collections = collections;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionImage other = (CollectionImage) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionImage [id=" + id + ", imageUrl=" + imageUrl + ", collections=" + collections + "]";
+	}
+    
 }
