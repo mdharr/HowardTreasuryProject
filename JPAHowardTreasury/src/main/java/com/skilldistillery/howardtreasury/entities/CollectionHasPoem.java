7674ca10d2781806db13050d2f7638a91af1e23d
Michael Harrington <mdharr7@gmail.com>
2023-09-08 15:45:53 +0800

    Stub out collection has poem entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPoem.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPoem.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -23,4 +25,63 @@ public class CollectionHasPoem {
     @JoinColumn(name = "poem_id")
     @MapsId("poemId")
     private Poem poem;
+
+	public CollectionHasPoem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CollectionHasPoem(CollectionHasPoemId id, Collection collection, Poem poem) {
+		super();
+		this.id = new CollectionHasPoemId(collection.getId(), poem.getId());
+		this.collection = collection;
+		this.poem = poem;
+	}
+
+	public CollectionHasPoemId getId() {
+		return id;
+	}
+
+	public void setId(CollectionHasPoemId id) {
+		this.id = id;
+	}
+
+	public Collection getCollection() {
+		return collection;
+	}
+
+	public void setCollection(Collection collection) {
+		this.collection = collection;
+	}
+
+	public Poem getPoem() {
+		return poem;
+	}
+
+	public void setPoem(Poem poem) {
+		this.poem = poem;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionHasPoem other = (CollectionHasPoem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionHasPoem [id=" + id + ", collection=" + collection + ", poem=" + poem + "]";
+	}
+    
 }
