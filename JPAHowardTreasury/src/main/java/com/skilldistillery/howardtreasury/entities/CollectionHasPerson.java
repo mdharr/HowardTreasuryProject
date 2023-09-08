a86a7d49cadfa48fcd7eae0a8ae42b988d7a4096
Michael Harrington <mdharr7@gmail.com>
2023-09-08 15:39:57 +0800

    Stub out embeddable id class for collection has person.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPerson.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPerson.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "collection_has_person")
 public class CollectionHasPerson {
 
+	@EmbeddedId
+	private CollectionHasStoryId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "collection_id")
+	@MapsId("collectionId")
+	private Collection collection;
+	
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    @MapsId("personId")
+    private Person person;
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPersonId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/CollectionHasPersonId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class CollectionHasPersonId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "collection_has_person")
+public class CollectionHasPersonId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "collection_id")
+	private int collectionId;
+	
+    @Column(name = "person_id")
+    private int personId;
+
+	public CollectionHasPersonId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CollectionHasPersonId(int collectionId, int personId) {
+		super();
+		this.collectionId = collectionId;
+		this.personId = personId;
+	}
+
+	public int getCollectionId() {
+		return collectionId;
+	}
+
+	public void setCollectionId(int collectionId) {
+		this.collectionId = collectionId;
+	}
+
+	public int getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(collectionId, personId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionHasPersonId other = (CollectionHasPersonId) obj;
+		return collectionId == other.collectionId && personId == other.personId;
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionHasPersonId [collectionId=" + collectionId + ", personId=" + personId + "]";
+	}
+    
 }
