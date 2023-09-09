06a9f542b80a027278774117e8175d984b1c3def
Michael Harrington <mdharr7@gmail.com>
2023-09-09 10:53:37 +0800

    Add many to many mapping with collection to illustrator entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Illustrator.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Illustrator.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Illustrator {
@@ -13,4 +18,18 @@ public class Illustrator {
 	private int id;
 	
 	private String name;
+	
+    @ManyToMany
+    @JoinTable(
+    		name = "collection_has_illustrator",
+    		joinColumns = @JoinColumn(name = "illustrator_id"),
+    		inverseJoinColumns = @JoinColumn(name = "collection_id")
+    		)
+    private List<Collection> collections;
+
+	public Illustrator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
