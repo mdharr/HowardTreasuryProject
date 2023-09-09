1dbeae932b05efebabbbc417f05d4445cdac7e4e
Michael Harrington <mdharr7@gmail.com>
2023-09-09 10:43:52 +0800

    Add illustrator entity to persistence.xml file to make hibernate aware.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Illustrator.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Illustrator.java
@@ -1,5 +1,16 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Illustrator {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
 }
