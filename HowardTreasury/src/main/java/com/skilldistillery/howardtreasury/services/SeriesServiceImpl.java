d5254e9767c4dc0d6d2646400ecc04f175d3f060
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:51:13 +0800

    Autowire corresponding repo in backend Series service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.SeriesRepository;
 
 @Service
 public class SeriesServiceImpl implements SeriesService {
 
+	@Autowired
+	private SeriesRepository seriesRepo;
 }
