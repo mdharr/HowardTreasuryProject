9927e8e6aebdfad76d73e6dd0272cebb0cb7a344
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:31:50 +0800

    Create repository for Illustrator entity.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/IllustratorRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.howardtreasury.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.howardtreasury.entities.Illustrator;
+
+public interface IllustratorRepository extends JpaRepository<Illustrator, Integer> {
+
+}
