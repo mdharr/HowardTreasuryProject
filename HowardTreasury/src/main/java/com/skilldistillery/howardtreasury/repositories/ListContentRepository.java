0b956cbdfd79c29f4a72e47bec9ebea7a1ebe008
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:25:19 +0800

    Create repository for ListContent.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/ListContentRepository.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.howardtreasury.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListContentRepository extends JpaRepository<T, ID> {
+
+}
