3b631be14ba24102f14082393a116fbeeeda664d
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:26:30 +0800

    Create repository for Story.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/StoryRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.howardtreasury.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.howardtreasury.entities.Story;
+
+public interface StoryRepository extends JpaRepository<Story, Integer> {
+
+}
