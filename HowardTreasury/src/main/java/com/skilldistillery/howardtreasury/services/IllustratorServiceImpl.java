d61bd7a47712230db6c4b2d9647c67198a01bf2a
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:53:37 +0800

    Autowire corresponding repo in backend Illustrator service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.IllustratorRepository;
 
 @Service
 public class IllustratorServiceImpl implements IllustratorService {
 
+	@Autowired
+	private IllustratorRepository illustratorRepo;
 }
