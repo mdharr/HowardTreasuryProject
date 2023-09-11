c1e4404536601288cd04063f4913f3f7067a2680
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:54:08 +0800

    Autowire corresponding repo in backend Collection service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.CollectionRepository;
 
 @Service
 public class CollectionServiceImpl implements CollectionService {
 
+	@Autowired
+	private CollectionRepository collectionRepo;
 }
