28fd55856e377c802d9f3173dc2d0594028fa5e2
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:52:11 +0800

    Autowire corresponding repo in backend Person service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.PersonRepository;
 
 @Service
 public class PersonServiceImpl implements PersonService {
 
+	@Autowired
+	private PersonRepository personRepo;
 }
