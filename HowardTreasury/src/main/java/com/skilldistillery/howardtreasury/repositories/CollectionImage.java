1c4381827f203ee9826d4f3007e9ea94150b6060
Michael Harrington <mdharr7@gmail.com>
2023-09-11 09:32:40 +0800

    Create repository for CollectionImage entity.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/CollectionImage.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.howardtreasury.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollectionImage extends JpaRepository<CollectionImage, Integer> {
+
+}
