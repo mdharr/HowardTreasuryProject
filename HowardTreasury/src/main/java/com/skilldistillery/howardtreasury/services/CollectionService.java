9bb76779a0c8715e46984d9451490fd8e1f320c7
Michael Harrington <mdharr7@gmail.com>
2023-09-12 11:05:26 +0800

    Add CRUD methods to Collection service interace.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionService.java
@@ -1,5 +1,18 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.Collection;
+
 public interface CollectionService {
 
+	public List<Collection> findAll();
+	
+	public Collection find(int collectionId);
+	
+	public Collection create(Collection collection);
+	
+	public Collection update(Collection collection);
+	
+	public void delete(int collectionId);
 }
