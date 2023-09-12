d3bce69e38c06ce505b3d650a1b694263799b154
Michael Harrington <mdharr7@gmail.com>
2023-09-12 11:08:01 +0800

    Write logic for find all and find by id methods in Collection service implementation. Find by id uses Optional.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.Collection;
 import com.skilldistillery.howardtreasury.repositories.CollectionRepository;
 
 @Service
@@ -10,5 +14,39 @@ public class CollectionServiceImpl implements CollectionService {
 
 	@Autowired
 	private CollectionRepository collectionRepo;
+
+	@Override
+	public List<Collection> findAll() {
+		return collectionRepo.findAll();
+	}
+
+	@Override
+	public Collection find(int collectionId) {
+		
+		Optional<Collection> collectionOpt = collectionRepo.findById(collectionId);
+		if(collectionOpt.isPresent()) {
+			Collection collection = collectionOpt.get();
+			return collection;
+		}
+		return null;
+	}
+
+	@Override
+	public Collection create(Collection collection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection update(Collection collection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int collectionId) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
