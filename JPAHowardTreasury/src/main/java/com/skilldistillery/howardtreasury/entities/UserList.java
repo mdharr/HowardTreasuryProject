3a970ff17f40a6368834e29eab40bd5839784af8
Michael Harrington <mdharr7@gmail.com>
2023-09-14 09:19:41 +0800

    Update UserList entity fields.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserList.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserList.java
@@ -8,12 +8,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "user_list")
@@ -30,21 +30,44 @@ public class UserList {
     @JoinColumn(name = "user_id")
 	private User user;
 	
-	@JsonManagedReference
-	@OneToMany(mappedBy = "userList")
-	private List<ListContent> listContents;
+    @ManyToMany
+    @JoinTable(
+        name = "user_list_has_story",
+        joinColumns = @JoinColumn(name = "user_list_id"),
+        inverseJoinColumns = @JoinColumn(name = "story_id")
+    )
+    private List<Story> stories;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_list_has_poem",
+        joinColumns = @JoinColumn(name = "user_list_id"),
+        inverseJoinColumns = @JoinColumn(name = "poem_id")
+    )
+    private List<Poem> poems;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_list_has_miscellanea",
+        joinColumns = @JoinColumn(name = "user_list_id"),
+        inverseJoinColumns = @JoinColumn(name = "miscellanea_id")
+    )
+    private List<Miscellanea> miscellaneas;
 
 	public UserList() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public UserList(int id, String name, User user, List<ListContent> listContents) {
+	public UserList(int id, String name, User user, List<Story> stories,
+			        List<Poem> poems, List<Miscellanea> miscellaneas) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.user = user;
-		this.listContents = listContents;
+		this.stories = stories;
+		this.poems = poems;
+		this.miscellaneas = miscellaneas;
 	}
 
 	public int getId() {
@@ -71,12 +94,28 @@ public class UserList {
 		this.user = user;
 	}
 
-	public List<ListContent> getListContents() {
-		return listContents;
+	public List<Story> getStories() {
+		return stories;
 	}
 
-	public void setListContents(List<ListContent> listContents) {
-		this.listContents = listContents;
+	public void setStories(List<Story> stories) {
+		this.stories = stories;
+	}
+
+	public List<Poem> getPoems() {
+		return poems;
+	}
+
+	public void setPoems(List<Poem> poems) {
+		this.poems = poems;
+	}
+
+	public List<Miscellanea> getMiscellaneas() {
+		return miscellaneas;
+	}
+
+	public void setMiscellaneas(List<Miscellanea> miscellaneas) {
+		this.miscellaneas = miscellaneas;
 	}
 
 	@Override
