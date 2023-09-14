83e4d3807ea12bf677c1254931c3f77a8c2a43d3
Michael Harrington <mdharr7@gmail.com>
2023-09-14 09:13:29 +0800

    Stub out user list has miscellanea embeddable id

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellanea.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellanea.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_list_has_miscellanea")
 public class UserListHasMiscellanea {
 
+	@EmbeddedId
+	private UserListHasMiscellaneaId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_list_id")
+	@MapsId("userListId")
+	private UserList userList;
+	
+    @ManyToOne
+    @JoinColumn(name = "miscellanea_id")
+    @MapsId("miscellaneaId")
+    private Miscellanea miscellanea;
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellaneaId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellaneaId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class UserListHasMiscellaneaId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "user_list_has_miscellanea")
+public class UserListHasMiscellaneaId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "user_list_id")
+	private int userListId;
+	
+    @Column(name = "miscellanea_id")
+    private int miscellaneaId;
+
+	public UserListHasMiscellaneaId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserListHasMiscellaneaId(int userListId, int miscellaneaId) {
+		super();
+		this.userListId = userListId;
+		this.miscellaneaId = miscellaneaId;
+	}
+
+	public int getUserListId() {
+		return userListId;
+	}
+
+	public void setUserListId(int userListId) {
+		this.userListId = userListId;
+	}
+
+	public int getMiscellaneaId() {
+		return miscellaneaId;
+	}
+
+	public void setMiscellaneaId(int miscellaneaId) {
+		this.miscellaneaId = miscellaneaId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(miscellaneaId, userListId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserListHasMiscellaneaId other = (UserListHasMiscellaneaId) obj;
+		return miscellaneaId == other.miscellaneaId && userListId == other.userListId;
+	}
+
+	@Override
+	public String toString() {
+		return "UserListHasMiscellaneaId [userListId=" + userListId + ", miscellaneaId=" + miscellaneaId + "]";
+	}
+    
 }
