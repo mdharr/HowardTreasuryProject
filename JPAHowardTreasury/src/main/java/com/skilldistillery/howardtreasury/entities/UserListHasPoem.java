959536d629b2cdff6e1ece6fecb14a1a851da8d8
Michael Harrington <mdharr7@gmail.com>
2023-09-14 09:09:15 +0800

    Stub out user has poem id embeddable.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasPoem.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasPoem.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_list_has_poem")
 public class UserListHasPoem {
 
+	@EmbeddedId
+	private UserListHasPoemId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_list_id")
+	@MapsId("userListId")
+	private UserList userList;
+	
+    @ManyToOne
+    @JoinColumn(name = "poem_id")
+    @MapsId("poemId")
+    private Poem poem;
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasPoemId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasPoemId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class UserListHasPoemId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "user_list_has_poem")
+public class UserListHasPoemId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "user_list_id")
+	private int userListId;
+	
+    @Column(name = "poem_id")
+    private int poemId;
+
+	public UserListHasPoemId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserListHasPoemId(int userListId, int poemId) {
+		super();
+		this.userListId = userListId;
+		this.poemId = poemId;
+	}
+
+	public int getUserListId() {
+		return userListId;
+	}
+
+	public void setUserListId(int userListId) {
+		this.userListId = userListId;
+	}
+
+	public int getPoemId() {
+		return poemId;
+	}
+
+	public void setPoemId(int poemId) {
+		this.poemId = poemId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(poemId, userListId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserListHasPoemId other = (UserListHasPoemId) obj;
+		return poemId == other.poemId && userListId == other.userListId;
+	}
+
+	@Override
+	public String toString() {
+		return "UserListHasPoemId [userListId=" + userListId + ", poemId=" + poemId + "]";
+	}
+    
 }
