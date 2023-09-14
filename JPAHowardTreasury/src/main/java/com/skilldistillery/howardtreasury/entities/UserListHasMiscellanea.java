ab71b8aa15ac02d90d114070a2941fb97f7e9ee3
Michael Harrington <mdharr7@gmail.com>
2023-09-14 09:14:36 +0800

    Stub out user list has miscellanea entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellanea.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/UserListHasMiscellanea.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -23,4 +25,63 @@ public class UserListHasMiscellanea {
     @JoinColumn(name = "miscellanea_id")
     @MapsId("miscellaneaId")
     private Miscellanea miscellanea;
+
+	public UserListHasMiscellanea() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserListHasMiscellanea(UserListHasMiscellaneaId id, UserList userList, Miscellanea miscellanea) {
+		super();
+		this.id = new UserListHasMiscellaneaId(userList.getId(), miscellanea.getId());
+		this.userList = userList;
+		this.miscellanea = miscellanea;
+	}
+
+	public UserListHasMiscellaneaId getId() {
+		return id;
+	}
+
+	public void setId(UserListHasMiscellaneaId id) {
+		this.id = id;
+	}
+
+	public UserList getUserList() {
+		return userList;
+	}
+
+	public void setUserList(UserList userList) {
+		this.userList = userList;
+	}
+
+	public Miscellanea getMiscellanea() {
+		return miscellanea;
+	}
+
+	public void setMiscellanea(Miscellanea miscellanea) {
+		this.miscellanea = miscellanea;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserListHasMiscellanea other = (UserListHasMiscellanea) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "UserListHasMiscellanea [id=" + id + ", userList=" + userList + ", miscellanea=" + miscellanea + "]";
+	}
+    
 }
