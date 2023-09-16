d95b769a2e499d6c6fddb1fed0b9137376189a51
Michael Harrington <mdharr7@gmail.com>
2023-09-16 12:00:34 +0800

    Implement service methods for indexing all collection images and retrieving a collection image by id.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionImageService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionImageService.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.CollectionImage;
+
 public interface CollectionImageService {
 
+	public List<CollectionImage> findAll();
+	
+	public CollectionImage find(int collectionImageId);
+	
+	public CollectionImage create(CollectionImage collectionImage);
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionImageServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/CollectionImageServiceImpl.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.CollectionImage;
 import com.skilldistillery.howardtreasury.repositories.CollectionImageRepository;
 
 @Service
@@ -10,4 +14,24 @@ public class CollectionImageServiceImpl implements CollectionImageService {
 
 	@Autowired
 	private CollectionImageRepository collectionImageRepo;
+
+	@Override
+	public List<CollectionImage> findAll() {
+		return collectionImageRepo.findAll();
+	}
+
+	@Override
+	public CollectionImage find(int collectionImageId) {
+		Optional<CollectionImage> collectionImageOpt = collectionImageRepo.findById(collectionImageId);
+		if(collectionImageOpt.isPresent()) {
+			CollectionImage collectionImage = collectionImageOpt.get();
+			return collectionImage;
+		}
+		return null;
+	}
+
+	@Override
+	public CollectionImage create(CollectionImage collectionImage) {
+		return null;
+	}
 }
