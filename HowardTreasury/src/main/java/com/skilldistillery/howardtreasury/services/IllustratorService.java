4f83c0c0ef7e618547ee12e52f158dc3539dfb50
Michael Harrington <mdharr7@gmail.com>
2023-09-17 10:55:58 +0800

    Implement service method for creating an illustrator and find all/find by id.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorService.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.Illustrator;
+
 public interface IllustratorService {
 
+	public List<Illustrator> findAll();
+	
+	public Illustrator find(int illustratorId);
+	
+	public Illustrator create(Illustrator illustrator);
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/IllustratorServiceImpl.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.Illustrator;
 import com.skilldistillery.howardtreasury.repositories.IllustratorRepository;
 
 @Service
@@ -10,4 +14,27 @@ public class IllustratorServiceImpl implements IllustratorService {
 
 	@Autowired
 	private IllustratorRepository illustratorRepo;
+
+	@Override
+	public List<Illustrator> findAll() {
+		return illustratorRepo.findAll();
+	}
+
+	@Override
+	public Illustrator find(int illustratorId) {
+		
+		Optional<Illustrator> illustratorOpt = illustratorRepo.findById(illustratorId);
+		if(illustratorOpt.isPresent()) {
+			Illustrator illustrator = illustratorOpt.get();
+			
+			return illustrator;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public Illustrator create(Illustrator illustrator) {
+		return illustratorRepo.save(illustrator);
+	}
 }
