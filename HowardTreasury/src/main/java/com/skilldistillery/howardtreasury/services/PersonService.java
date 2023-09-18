055124b8b5bdf1d06efa466132bc9aaad6b90bc7
Michael Harrington <mdharr7@gmail.com>
2023-09-18 15:57:51 +0800

    Add find all method to person service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonService.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.Person;
+
 public interface PersonService {
 
+	public List<Person> findAll();
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/PersonServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.Person;
 import com.skilldistillery.howardtreasury.repositories.PersonRepository;
 
 @Service
@@ -10,4 +13,9 @@ public class PersonServiceImpl implements PersonService {
 
 	@Autowired
 	private PersonRepository personRepo;
+
+	@Override
+	public List<Person> findAll() {
+		return personRepo.findAll();
+	}
 }
