b3f9eec9e6977cfe83a7d311f53340f1461ec9f1
Michael Harrington <mdharr7@gmail.com>
2023-09-18 15:59:15 +0800

    Add find all method to series service.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesService.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.Series;
+
 public interface SeriesService {
 
+	public List<Series> findAll();
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/SeriesServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.howardtreasury.entities.Series;
 import com.skilldistillery.howardtreasury.repositories.SeriesRepository;
 
 @Service
@@ -10,4 +13,9 @@ public class SeriesServiceImpl implements SeriesService {
 
 	@Autowired
 	private SeriesRepository seriesRepo;
+
+	@Override
+	public List<Series> findAll() {
+		return seriesRepo.findAll();
+	}
 }
