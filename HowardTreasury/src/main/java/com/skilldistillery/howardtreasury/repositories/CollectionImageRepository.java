d2ef56b2f8109dd8457277207df92f8d6b707a08
Michael Harrington <mdharr7@gmail.com>
2023-09-19 16:16:59 +0800

    Add method in collection image repo to find collection images by collections id

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/CollectionImageRepository.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/CollectionImageRepository.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.howardtreasury.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.howardtreasury.entities.CollectionImage;
 
 public interface CollectionImageRepository extends JpaRepository<CollectionImage, Integer> {
 
+	List<CollectionImage> findByCollections_Id(int collectionId);
 }
