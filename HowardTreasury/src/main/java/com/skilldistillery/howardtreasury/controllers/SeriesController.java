6808264c0611d69db17a00c6facad135de37aa22
Michael Harrington <mdharr7@gmail.com>
2023-09-19 10:32:26 +0800

    Add api request mapping to all controllers.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/SeriesController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/SeriesController.java
@@ -2,12 +2,14 @@ package com.skilldistillery.howardtreasury.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.howardtreasury.services.SeriesService;
 
 @RestController
 @CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
 public class SeriesController {
 
 	@Autowired
