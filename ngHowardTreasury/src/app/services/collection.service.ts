ae14f6001de54318fbf89c0f02752dab1ddaa247
Michael Harrington <mdharr7@gmail.com>
2023-09-21 14:58:13 +0800

    Stub out frontend service for collection, along with indexAll method which returns an array of collection objects as an observable.

--- ngHowardTreasury/src/app/services/collection.service.ts
+++ ngHowardTreasury/src/app/services/collection.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Collection } from '../models/collection';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionService {
+  private url = environment.baseUrl + 'api/collections';
 
-  constructor() { }
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Collection[]> {
+    return this.http.get<Collection[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('CollectionService.indexAll(): error retrieving list of collections ' + err)
+        );
+      })
+    );
+  }
 }
