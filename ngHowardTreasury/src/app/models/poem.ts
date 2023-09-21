de7cb06ecd0a62ca270a6304a53639458548898a
Michael Harrington <mdharr7@gmail.com>
2023-09-21 13:08:45 +0800

    Stub out poem model.

--- ngHowardTreasury/src/app/models/poem.ts
+++ ngHowardTreasury/src/app/models/poem.ts
@@ -1,2 +1,24 @@
+import { Collection } from "./collection";
+
 export class Poem {
+
+  id: number;
+  title: string;
+  textUrl: string;
+  // userLists: UserList[];
+  collections: Collection[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    textUrl: string = '',
+    // userLists: UserList[] = [],
+    collections: Collection[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    this.textUrl = textUrl;
+    // this.userLists = userLists;
+    this.collections = collections;
+  }
 }
