fae57910cbc64a401e80e4dfd71c73e3b5fffa44
Michael Harrington <mdharr7@gmail.com>
2023-09-21 13:05:03 +0800

    Stub out collection image model.

--- ngHowardTreasury/src/app/models/collection-image.ts
+++ ngHowardTreasury/src/app/models/collection-image.ts
@@ -1,2 +1,18 @@
+import { Collection } from "./collection";
+
 export class CollectionImage {
+
+  id: number;
+  imageUrl: string;
+  collections: Collection[];
+
+  constructor(
+    id: number = 0,
+    imageUrl: string = '',
+    collections: Collection[] = []
+  ) {
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.collections = collections;
+  }
 }
