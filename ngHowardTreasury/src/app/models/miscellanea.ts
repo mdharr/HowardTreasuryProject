cd241c9b6133554b503fc169d549a6d1ef4593ad
Michael Harrington <mdharr7@gmail.com>
2023-09-21 13:09:39 +0800

    Stub out miscellanea model.

--- ngHowardTreasury/src/app/models/miscellanea.ts
+++ ngHowardTreasury/src/app/models/miscellanea.ts
@@ -1,2 +1,20 @@
+import { Collection } from "./collection";
+
 export class Miscellanea {
+  id: number;
+  title: string;
+  // userLists: UserList[];
+  collections: Collection[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    // userLists: UserList[] = [],
+    collections: Collection[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    // this.userLists = userLists;
+    this.collections = collections;
+  }
 }
