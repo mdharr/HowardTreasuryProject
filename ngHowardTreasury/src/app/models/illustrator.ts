cc8e0e4e593b57275c20495e6ee441c00dbb565e
Michael Harrington <mdharr7@gmail.com>
2023-09-21 13:03:22 +0800

    Stub out illustrator model.

--- ngHowardTreasury/src/app/models/illustrator.ts
+++ ngHowardTreasury/src/app/models/illustrator.ts
@@ -1,2 +1,18 @@
+import { Collection } from "./collection";
+
 export class Illustrator {
+
+  id: number;
+  name: string;
+  collections: Collection[];
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    collections: Collection[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.collections = collections;
+  }
 }
