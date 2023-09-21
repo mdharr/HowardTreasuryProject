b6d1c0c6a78fcbe0319305a9d9c7625047dc9485
Michael Harrington <mdharr7@gmail.com>
2023-09-21 13:01:53 +0800

    Stub out series model.

--- ngHowardTreasury/src/app/models/series.ts
+++ ngHowardTreasury/src/app/models/series.ts
@@ -1,2 +1,18 @@
+import { Collection } from "./collection";
+
 export class Series {
+
+  id: number;
+  title: string;
+  collections: Collection[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    collections: Collection[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    this.collections = collections;
+  }
 }
