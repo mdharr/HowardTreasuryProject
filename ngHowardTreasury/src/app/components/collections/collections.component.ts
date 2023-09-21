e084d06e66b2feee0fc3f9e13d9e7a143e735a2a
Michael Harrington <mdharr7@gmail.com>
2023-09-21 15:05:02 +0800

    Subscribe to indexAll observable from collection service in collection component.

--- ngHowardTreasury/src/app/components/collections/collections.component.ts
+++ ngHowardTreasury/src/app/components/collections/collections.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Collection } from 'src/app/models/collection';
+import { AuthService } from 'src/app/services/auth.service';
+import { CollectionService } from 'src/app/services/collection.service';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.css']
 })
-export class CollectionsComponent {
+export class CollectionsComponent implements OnInit, OnDestroy {
+
+  collections: Collection[] = [];
+
+  private collectionSubscription: Subscription | undefined;
+
+  auth = inject(AuthService);
+  collectionService = inject(CollectionService);
+
+  ngOnInit(): void {
+
+    this.collectionSubscription = this.collectionService.indexAll().subscribe({
+      next: (data) => {
+        this.collections = data;
+      },
+      error:(fail) => {
+        console.error('Error retrieving collections');
+        console.error(fail);
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.collectionSubscription) {
+      this.collectionSubscription.unsubscribe();
+    }
+  }
+
 
 }
