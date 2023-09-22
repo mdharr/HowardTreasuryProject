1adb920588ec9a8c884ffab89ba6f4afefbe6b35
Michael Harrington <mdharr7@gmail.com>
2023-09-22 16:02:29 +0800

    Stub out poems component ts file.

--- ngHowardTreasury/src/app/components/poems/poems.component.ts
+++ ngHowardTreasury/src/app/components/poems/poems.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Poem } from 'src/app/models/poem';
+import { AuthService } from 'src/app/services/auth.service';
+import { PoemService } from 'src/app/services/poem.service';
 
 @Component({
   selector: 'app-poems',
   templateUrl: './poems.component.html',
   styleUrls: ['./poems.component.css']
 })
-export class PoemsComponent {
+export class PoemsComponent implements OnInit, OnDestroy {
 
+  // properties initialization
+  poems: Poem[] = [];
+
+  // subscriptions declarations
+  private poemSubscription: Subscription | undefined;
+
+  // service injections
+  auth = inject(AuthService);
+  poemService = inject(PoemService);
+
+  ngOnInit(): void {
+
+    this.poemSubscription = this.poemService.indexAll().subscribe({
+      next: (data) => {
+        this.poems = data;
+      },
+      error:(fail) => {
+        console.error('Error retrieving poems');
+        console.error(fail);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.poemSubscription) {
+      this.poemSubscription.unsubscribe();
+    }
+  }
 }
