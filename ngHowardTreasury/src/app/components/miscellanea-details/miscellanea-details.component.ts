a9d8934637607ae80a2a5187953c5f5e60a089b0
Michael Harrington <mdharr7@gmail.com>
2023-09-22 16:08:16 +0800

    Stub out miscellanea component html file.

--- ngHowardTreasury/src/app/components/miscellanea-details/miscellanea-details.component.ts
+++ ngHowardTreasury/src/app/components/miscellanea-details/miscellanea-details.component.ts
@@ -9,34 +9,6 @@ import { MiscellaneaService } from 'src/app/services/miscellanea.service';
   templateUrl: './miscellanea-details.component.html',
   styleUrls: ['./miscellanea-details.component.css']
 })
-export class MiscellaneaDetailsComponent implements OnInit, OnDestroy {
+export class MiscellaneaDetailsComponent {
 
-    // properties initialization
-    miscellaneas: Miscellanea[] = [];
-
-    // subscriptions declarations
-    private miscellaneaSubscription: Subscription | undefined;
-
-    // service injections
-    auth = inject(AuthService);
-    miscellaneaService = inject(MiscellaneaService);
-
-    ngOnInit(): void {
-
-      this.miscellaneaSubscription = this.miscellaneaService.indexAll().subscribe({
-        next: (data) => {
-          this.miscellaneas = data;
-        },
-        error:(fail) => {
-          console.error('Error retrieving miscellaneas');
-          console.error(fail);
-        }
-      });
-    }
-
-    ngOnDestroy(): void {
-      if (this.miscellaneaSubscription) {
-        this.miscellaneaSubscription.unsubscribe();
-      }
-    }
 }
--- ngHowardTreasury/src/app/components/miscellanea/miscellanea.component.ts
+++ ngHowardTreasury/src/app/components/miscellanea/miscellanea.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Miscellanea } from 'src/app/models/miscellanea';
+import { AuthService } from 'src/app/services/auth.service';
+import { MiscellaneaService } from 'src/app/services/miscellanea.service';
 
 @Component({
   selector: 'app-miscellanea',
   templateUrl: './miscellanea.component.html',
   styleUrls: ['./miscellanea.component.css']
 })
-export class MiscellaneaComponent {
+export class MiscellaneaComponent implements OnInit, OnDestroy {
 
+    // properties initialization
+    miscellaneas: Miscellanea[] = [];
+
+    // subscriptions declarations
+    private miscellaneaSubscription: Subscription | undefined;
+
+    // service injections
+    auth = inject(AuthService);
+    miscellaneaService = inject(MiscellaneaService);
+
+    ngOnInit(): void {
+
+      this.miscellaneaSubscription = this.miscellaneaService.indexAll().subscribe({
+        next: (data) => {
+          this.miscellaneas = data;
+        },
+        error:(fail) => {
+          console.error('Error retrieving miscellaneas');
+          console.error(fail);
+        }
+      });
+    }
+
+    ngOnDestroy(): void {
+      if (this.miscellaneaSubscription) {
+        this.miscellaneaSubscription.unsubscribe();
+      }
+    }
 }
