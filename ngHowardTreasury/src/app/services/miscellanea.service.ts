71adcb7304275a30f42d12854a09159feabba98b
Michael Harrington <mdharr7@gmail.com>
2023-09-22 16:00:01 +0800

    Stub out frontend miscellanea service methods indexAll and find by id.

--- ngHowardTreasury/src/app/services/miscellanea.service.ts
+++ ngHowardTreasury/src/app/services/miscellanea.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Miscellanea } from '../models/miscellanea';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MiscellaneaService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/miscellaneas';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Miscellanea[]> {
+    return this.http.get<Miscellanea[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('MiscellaneaService.indexAll(): error retrieving list of miscellanea ' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Miscellanea> {
+    return this.http.get<Miscellanea>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('MiscellaneaService.find(): error retrieving miscellanea: ' + err)
+        );
+      })
+    );
+  }
 }
