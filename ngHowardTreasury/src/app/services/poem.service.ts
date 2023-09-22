b3bfb8a51adaa5d5a198a94bf8068cd6e3cb48c4
Michael Harrington <mdharr7@gmail.com>
2023-09-22 15:57:14 +0800

    Stub out frontend poem service methods indexAll and find by id.

--- ngHowardTreasury/src/app/services/poem.service.ts
+++ ngHowardTreasury/src/app/services/poem.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Poem } from '../models/poem';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoemService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/poems';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Poem[]> {
+    return this.http.get<Poem[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('PoemService.indexAll(): error retrieving list of poems ' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Poem> {
+    return this.http.get<Poem>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('PoemService.find(): error retrieving poem: ' + err)
+        );
+      })
+    );
+  }
 }
