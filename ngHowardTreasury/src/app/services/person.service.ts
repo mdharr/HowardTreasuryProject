36c8c982c3f241437e798872fc04c7fa2e9e3c26
Michael Harrington <mdharr7@gmail.com>
2023-09-22 15:58:40 +0800

    Stub out frontend person service methods indexAll and find by id.

--- ngHowardTreasury/src/app/services/person.service.ts
+++ ngHowardTreasury/src/app/services/person.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Person } from '../models/person';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/persons';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('PersonService.indexAll(): error retrieving list of persons ' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Person> {
+    return this.http.get<Person>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('PersonService.find(): error retrieving person: ' + err)
+        );
+      })
+    );
+  }
 }
