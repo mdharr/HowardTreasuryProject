5755a6b678ed098b673a1b88c65d4a2ed323637e
Michael Harrington <mdharr7@gmail.com>
2023-09-22 16:05:55 +0800

    Stub out characters component ts file.

--- ngHowardTreasury/src/app/components/characters/characters.component.ts
+++ ngHowardTreasury/src/app/components/characters/characters.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Person } from 'src/app/models/person';
+import { AuthService } from 'src/app/services/auth.service';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.css']
 })
-export class CharactersComponent {
+export class CharactersComponent implements OnInit, OnDestroy {
 
+  // properties initialization
+  persons: Person[] = [];
+
+  // subscriptions declarations
+  private personSubscription: Subscription | undefined;
+
+  // service injections
+  auth = inject(AuthService);
+  personService = inject(PersonService);
+
+  ngOnInit(): void {
+
+    this.personSubscription = this.personService.indexAll().subscribe({
+      next: (data) => {
+        this.persons = data;
+      },
+      error:(fail) => {
+        console.error('Error retrieving persons');
+        console.error(fail);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.personSubscription) {
+      this.personSubscription.unsubscribe();
+    }
+  }
 }
