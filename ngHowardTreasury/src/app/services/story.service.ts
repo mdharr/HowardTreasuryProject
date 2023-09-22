fa69e17dd8b4bb5856e8bb6de86c4dea9a6ab31d
Michael Harrington <mdharr7@gmail.com>
2023-09-22 15:06:58 +0800

    Stub out frontend story service methods.

--- ngHowardTreasury/src/app/services/story.service.ts
+++ ngHowardTreasury/src/app/services/story.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Story } from '../models/story';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoryService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/stories';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Story[]> {
+    return this.http.get<Story[]>(this.url).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('StoryService.indexAll(): error retrieving list of stories ' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Story> {
+    return this.http.get<Story>(`${this.url}/${id}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('StoryService.find(): error retrieving story: ' + err)
+        );
+      })
+    );
+  }
 }
