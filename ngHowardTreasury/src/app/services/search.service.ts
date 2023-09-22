625a383d20e84232f213d4edd244e91d131206f6
Michael Harrington <mdharr7@gmail.com>
2023-09-22 19:09:00 +0800

    Stub out frontend search service.

--- ngHowardTreasury/src/app/services/search.service.ts
+++ ngHowardTreasury/src/app/services/search.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/search';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  search(query: string): Observable<any[]> {
+    // Make HTTP GET request to your search endpoint
+    return this.http.get<any[]>(`${this.url}?query=${query}`);
+  }
+
 }
