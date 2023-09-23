de3b5c971a0d13ac95e80ae1150024b02e33958c
Michael Harrington <mdharr7@gmail.com>
2023-09-23 22:48:44 +0800

    Implement delay function in collections component as workaround to poor performance from trying to animate several items at once while simultaneously collapsing the navbar menu.

--- ngHowardTreasury/src/app/components/collections/collections.component.ts
+++ ngHowardTreasury/src/app/components/collections/collections.component.ts
@@ -12,6 +12,7 @@ import { CollectionService } from 'src/app/services/collection.service';
 export class CollectionsComponent implements OnInit, OnDestroy {
 
   collections: Collection[] = [];
+  loading: boolean = true;
 
   private collectionSubscription: Subscription | undefined;
 
@@ -20,14 +21,18 @@ export class CollectionsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
 
-    this.collectionSubscription = this.collectionService.indexAll().subscribe({
-      next: (data) => {
-        this.collections = data;
-      },
-      error:(fail) => {
-        console.error('Error retrieving collections');
-        console.error(fail);
-      }
+    this.delay(200).then(() => {
+      this.collectionSubscription = this.collectionService.indexAll().subscribe({
+        next: (data) => {
+          this.collections = data;
+          this.loading = false; // Set loading to false when data is available
+        },
+        error: (fail) => {
+          console.error('Error retrieving collections');
+          console.error(fail);
+          this.loading = false; // Set loading to false in case of an error
+        }
+      });
     });
   }
 
@@ -35,6 +40,14 @@ export class CollectionsComponent implements OnInit, OnDestroy {
     if (this.collectionSubscription) {
       this.collectionSubscription.unsubscribe();
     }
+  }
+
+  delay(ms: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
   }
 
 }
