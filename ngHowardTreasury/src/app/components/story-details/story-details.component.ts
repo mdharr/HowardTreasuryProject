6f2e3f5c9f7ecd241c56b487bf3c6844dcd751c5
Michael Harrington <mdharr7@gmail.com>
2023-09-25 11:46:12 +0800

    Stub out story details component and partially stub out styling.

--- ngHowardTreasury/src/app/components/story-details/story-details.component.ts
+++ ngHowardTreasury/src/app/components/story-details/story-details.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Story } from 'src/app/models/story';
+import { AuthService } from 'src/app/services/auth.service';
+import { StoryService } from 'src/app/services/story.service';
 
 @Component({
   selector: 'app-story-details',
   templateUrl: './story-details.component.html',
   styleUrls: ['./story-details.component.css']
 })
-export class StoryDetailsComponent {
+export class StoryDetailsComponent implements OnInit, OnDestroy {
 
+    // property initialization
+    storyId: number = 0;
+    story: Story = new Story();
+    storyExcerpt: string = '';
+
+    // booleans
+
+    // service injection
+    auth = inject(AuthService);
+    storyService = inject(StoryService);
+    activatedRoute = inject(ActivatedRoute);
+    renderer = inject(Renderer2);
+
+    // subscription declaration
+    private paramsSubscription: Subscription | undefined;
+    private storySubscription: Subscription | undefined;
+
+    ngOnInit(): void {
+      this.getRouteParams();
+      this.subscribeToStoryServiceById();
+    }
+
+    ngOnDestroy(): void {
+      this.destroySubscriptions();
+    }
+
+    getRouteParams = () => {
+      this.paramsSubscription = this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+        let idString = params.get('storyId');
+        if(idString) {
+          this.storyId = +idString;
+        }
+      });
+    }
+
+    subscribeToStoryServiceById = () => {
+      this.storySubscription = this.storyService.find(this.storyId).subscribe({
+        next: (data) => {
+          this.story = data;
+          this.storyExcerpt = this.createIlluminatedInitial(data.excerpt);
+        },
+        error: (fail) => {
+          console.error('Error getting story');
+          console.error(fail);
+        }
+      });
+    }
+
+    destroySubscriptions = () => {
+      if(this.paramsSubscription) {
+        this.paramsSubscription.unsubscribe();
+      }
+
+      if(this.storySubscription) {
+        this.storySubscription.unsubscribe();
+      }
+    }
+
+    createIlluminatedInitial = (text: string): string => {
+      if (text.length === 0) {
+        return '';
+      }
+
+      const firstLetter = text.charAt(0);
+      const restOfString = text.slice(1);
+
+      const illuminatedInitial = `<span class="first-letter">${firstLetter}</span>`;
+      return illuminatedInitial + restOfString;
+    }
 }
