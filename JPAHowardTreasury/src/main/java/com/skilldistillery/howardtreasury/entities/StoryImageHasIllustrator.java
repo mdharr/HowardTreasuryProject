ccb30ae3254d28be5fc145e1360a40c6b9af3d8f
Michael Harrington <mdharr7@gmail.com>
2023-09-28 10:05:15 +0800

    Stub out story image has illustrator entity and embeddable.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryImageHasIllustrator.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryImageHasIllustrator.java
@@ -1,5 +1,88 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "story_image_has_illustrator")
 public class StoryImageHasIllustrator {
 
+	@EmbeddedId
+	private StoryImageHasIllustratorId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "story_image_id")
+	@MapsId("storyImageId")
+	private StoryImage storyImage;
+	
+	@ManyToOne
+	@JoinColumn(name = "illustrator_id")
+	@MapsId("illustratorId")
+	private Illustrator illustrator;
+
+	public StoryImageHasIllustrator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryImageHasIllustrator(StoryImageHasIllustratorId id, StoryImage storyImage, Illustrator illustrator) {
+		super();
+		this.id = new StoryImageHasIllustratorId(storyImage.getId(), illustrator.getId());
+		this.storyImage = storyImage;
+		this.illustrator = illustrator;
+	}
+
+	public StoryImageHasIllustratorId getId() {
+		return id;
+	}
+
+	public void setId(StoryImageHasIllustratorId id) {
+		this.id = id;
+	}
+
+	public StoryImage getStoryImage() {
+		return storyImage;
+	}
+
+	public void setStoryImage(StoryImage storyImage) {
+		this.storyImage = storyImage;
+	}
+
+	public Illustrator getIllustrator() {
+		return illustrator;
+	}
+
+	public void setIllustrator(Illustrator illustrator) {
+		this.illustrator = illustrator;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryImageHasIllustrator other = (StoryImageHasIllustrator) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "StoryImageHasIllustrator [id=" + id + ", storyImage=" + storyImage + ", illustrator=" + illustrator
+				+ "]";
+	}
+	
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryImageHasIllustratorId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryImageHasIllustratorId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class StoryImageHasIllustratorId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "story_image_has_illustrator")
+public class StoryImageHasIllustratorId implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "story_image_id")
+	private int storyImageId;
+	
+	@Column(name = "illustrator_id")
+	private int illustratorId;
+
+	public StoryImageHasIllustratorId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryImageHasIllustratorId(int storyImageId, int illustratorId) {
+		super();
+		this.storyImageId = storyImageId;
+		this.illustratorId = illustratorId;
+	}
+
+	public int getStoryImageId() {
+		return storyImageId;
+	}
+
+	public void setStoryImageId(int storyImageId) {
+		this.storyImageId = storyImageId;
+	}
+
+	public int getIllustratorId() {
+		return illustratorId;
+	}
+
+	public void setIllustratorId(int illustratorId) {
+		this.illustratorId = illustratorId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(illustratorId, storyImageId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryImageHasIllustratorId other = (StoryImageHasIllustratorId) obj;
+		return illustratorId == other.illustratorId && storyImageId == other.storyImageId;
+	}
+
+	@Override
+	public String toString() {
+		return "StoryImageHasIllustratorId [storyImageId=" + storyImageId + ", illustratorId=" + illustratorId + "]";
+	}
+	
 }
