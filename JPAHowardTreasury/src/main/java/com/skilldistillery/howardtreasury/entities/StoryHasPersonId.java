bddd4c9603d9627aeb59e76dacb0a094407c7902
Michael Harrington <mdharr7@gmail.com>
2023-09-28 10:15:49 +0800

    Finish stubbing out story has person embeddable class.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasPersonId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasPersonId.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -17,5 +18,58 @@ public class StoryHasPersonId implements Serializable {
 	
 	@Column(name = "person_id")
 	private int personId;
+
+	public StoryHasPersonId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryHasPersonId(int storyId, int personId) {
+		super();
+		this.storyId = storyId;
+		this.personId = personId;
+	}
+
+	public int getStoryId() {
+		return storyId;
+	}
+
+	public void setStoryId(int storyId) {
+		this.storyId = storyId;
+	}
+
+	public int getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(personId, storyId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryHasPersonId other = (StoryHasPersonId) obj;
+		return personId == other.personId && storyId == other.storyId;
+	}
+
+	@Override
+	public String toString() {
+		return "StoryHasPersonId [storyId=" + storyId + ", personId=" + personId + "]";
+	}
 
 }
