b27f22edf956ef04d94b17aa3066527616dc4c96
Michael Harrington <mdharr7@gmail.com>
2023-09-28 09:28:24 +0800

    Update person entity in jpa project.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Person.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Person.java
@@ -4,17 +4,14 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Person {
@@ -24,6 +21,11 @@ public class Person {
 	private int id;
 	
 	private String name;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+	
+	private String description;
 	
 	@JsonIgnore
 	@ManyToMany(mappedBy = "persons", cascade = CascadeType.MERGE)
@@ -55,6 +57,22 @@ public class Person {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public List<Collection> getCollections() {
