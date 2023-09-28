b32ae059b8dd6d8c831e872148b91f9e3deb93d7
Michael Harrington <mdharr7@gmail.com>
2023-09-28 10:17:52 +0800

    Finish stubbing out story has person entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasPerson.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasPerson.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -20,7 +22,66 @@ public class StoryHasPerson {
 	private Story story;
 	
 	@ManyToOne
-	@JoinColumn("person_id")
+	@JoinColumn(name = "person_id")
 	@MapsId("personId")
 	private Person person;
+
+	public StoryHasPerson() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryHasPerson(StoryHasPersonId id, Story story, Person person) {
+		super();
+		this.id = new StoryHasPersonId(story.getId(), person.getId());
+		this.story = story;
+		this.person = person;
+	}
+
+	public StoryHasPersonId getId() {
+		return id;
+	}
+
+	public void setId(StoryHasPersonId id) {
+		this.id = id;
+	}
+
+	public Story getStory() {
+		return story;
+	}
+
+	public void setStory(Story story) {
+		this.story = story;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryHasPerson other = (StoryHasPerson) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "StoryHasPerson [id=" + id + ", story=" + story + ", person=" + person + "]";
+	}
+	
 }
