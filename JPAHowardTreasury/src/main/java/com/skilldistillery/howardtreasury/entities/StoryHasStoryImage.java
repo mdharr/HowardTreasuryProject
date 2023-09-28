fc4614ba38eef15ff688c335f6f10d4cf96fdef1
Michael Harrington <mdharr7@gmail.com>
2023-09-28 09:55:16 +0800

    Stub out story has story image entity and embeddable id classes. Add to persistence.xml file to make hibernate aware.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasStoryImage.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasStoryImage.java
@@ -1,5 +1,87 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "story_has_story_image")
 public class StoryHasStoryImage {
+	
+	@EmbeddedId
+	private StoryHasStoryImageId id;
+	
+	@ManyToOne
+	@JoinColumn(name = "story_id")
+	@MapsId("storyId")
+	private Story story;
+	
+    @ManyToOne
+    @JoinColumn(name = "story_image_id")
+    @MapsId("storyImageId")
+    private StoryImage storyImage;
+
+	public StoryHasStoryImage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryHasStoryImage(StoryHasStoryImageId id, Story story, StoryImage storyImage) {
+		super();
+		this.id = new StoryHasStoryImageId(story.getId(), storyImage.getId());
+		this.story = story;
+		this.storyImage = storyImage;
+	}
+
+	public StoryHasStoryImageId getId() {
+		return id;
+	}
+
+	public void setId(StoryHasStoryImageId id) {
+		this.id = id;
+	}
+
+	public Story getStory() {
+		return story;
+	}
+
+	public void setStory(Story story) {
+		this.story = story;
+	}
+
+	public StoryImage getStoryImage() {
+		return storyImage;
+	}
+
+	public void setStoryImage(StoryImage storyImage) {
+		this.storyImage = storyImage;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryHasStoryImage other = (StoryHasStoryImage) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "StoryHasStoryImage [id=" + id + ", story=" + story + ", storyImage=" + storyImage + "]";
+	}
 
 }
--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasStoryImageId.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/StoryHasStoryImageId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.howardtreasury.entities;
 
-public class StoryHasStoryImageId {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "story_has_story_image_id")
+public class StoryHasStoryImageId implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "story_id")
+	private int storyId;
+	
+    @Column(name = "story_image_id")
+    private int storyImageId;
+
+	public StoryHasStoryImageId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoryHasStoryImageId(int storyId, int storyImageId) {
+		super();
+		this.storyId = storyId;
+		this.storyImageId = storyImageId;
+	}
+
+	public int getStoryId() {
+		return storyId;
+	}
+
+	public void setStoryId(int storyId) {
+		this.storyId = storyId;
+	}
+
+	public int getStoryImageId() {
+		return storyImageId;
+	}
+
+	public void setStoryImageId(int storyImageId) {
+		this.storyImageId = storyImageId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(storyId, storyImageId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StoryHasStoryImageId other = (StoryHasStoryImageId) obj;
+		return storyId == other.storyId && storyImageId == other.storyImageId;
+	}
+
+	@Override
+	public String toString() {
+		return "StoryHasStoryImageId [storyId=" + storyId + ", storyImageId=" + storyImageId + "]";
+	}
 
 }
