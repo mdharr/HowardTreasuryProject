0e7e9bbcc7396e116ab35d00f06443b1b32736f9
Michael Harrington <mdharr7@gmail.com>
2023-10-02 15:11:07 +0800

    Add new method in collection repository for retrieving a list of collections by miscellanea id.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/CollectionRepository.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/CollectionRepository.java
@@ -14,4 +14,6 @@ public interface CollectionRepository extends JpaRepository<Collection, Integer>
 	
 	List<Collection> findByPoems_Id(int poemId);
 	
+	List<Collection> findByMiscellaneas_Id(int miscellaneaId);
+	
 }
