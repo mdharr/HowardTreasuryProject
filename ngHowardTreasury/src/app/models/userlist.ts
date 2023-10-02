59c2c8bc5c088bda322acbf4fe5a0401ecaa1ad1
Michael Harrington <mdharr7@gmail.com>
2023-10-02 11:30:45 +0800

    Stub out frontend userlist model.

--- ngHowardTreasury/src/app/models/userlist.ts
+++ ngHowardTreasury/src/app/models/userlist.ts
@@ -1,2 +1,30 @@
+import { Miscellanea } from "./miscellanea";
+import { Poem } from "./poem";
+import { Story } from "./story";
+import { User } from "./user";
+
 export class Userlist {
+
+  id: number;
+  name: string;
+  user: User;
+  stories: Story[];
+  poems: Poem[];
+  miscellaneas: Miscellanea[];
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    user: User = new User(),
+    stories: Story[] = [],
+    poems: Poem[] = [],
+    miscellaneas: Miscellanea[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.user = user;
+    this.stories = stories;
+    this.poems = poems;
+    this.miscellaneas = miscellaneas;
+  }
 }
