ef91aaa29cadd10930d238d5168ed5d347906566
Michael Harrington <mdharr7@gmail.com>
2023-10-13 10:33:28 +0800

    Stub out create user list dialog component typescript.

--- ngHowardTreasury/src/app/components/create-user-list-dialog/create-user-list-dialog.component.ts
+++ ngHowardTreasury/src/app/components/create-user-list-dialog/create-user-list-dialog.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { UserList } from 'src/app/models/user-list';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserlistService } from 'src/app/services/userlist.service';
 
 @Component({
   selector: 'app-create-user-list-dialog',
@@ -6,5 +13,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user-list-dialog.component.css']
 })
 export class CreateUserListDialogComponent {
+    // property initialization
+    loggedInUser: User = new User();
+    userList: UserList = new UserList();
+    newUserListName: string = '';
 
+    // booleans
+    isLoggedIn: boolean = false;
+
+    // subscriptions
+    private authSubscription: Subscription | undefined;
+
+    // service injection
+    route = inject(ActivatedRoute);
+    router = inject(Router);
+    authService = inject(AuthService);
+    userListService = inject(UserlistService);
+
+    constructor(
+      private dialogRef: MatDialogRef<CreateUserListDialogComponent>,
+      @Inject(MAT_DIALOG_DATA) private data: any
+    ) { }
+
+    ngOnInit(): void {
+
+      this.subscribeToAuth();
+
+    }
+
+    subscribeToAuth = () => {
+      this.authSubscription = this.authService.getLoggedInUser().subscribe({
+        next: (user) => {
+          this.loggedInUser = user;
+
+        },
+        error: (error) => {
+          console.log('Error getting loggedInUser');
+          console.log(error);
+        },
+      });
+    }
+
+    ngOnDestroy(): void {
+      if (this.authSubscription) {
+        this.authSubscription.unsubscribe();
+      }
+    }
+
+    closeDialog() {
+      this.dialogRef.close();
+    }
 }
