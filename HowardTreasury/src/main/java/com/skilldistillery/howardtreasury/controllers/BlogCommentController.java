9455be275a376fd0d1ea86e07a3e3808be6f9c74
Michael Harrington <mdharr7@gmail.com>
2023-10-16 18:03:05 +0800

    Write out blog comment controller methods to expose services as RESTful endpoints.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogCommentController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogCommentController.java
@@ -1,10 +1,22 @@
 package com.skilldistillery.howardtreasury.controllers;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.howardtreasury.entities.BlogComment;
 import com.skilldistillery.howardtreasury.services.BlogCommentService;
 
 @RestController
@@ -14,4 +26,87 @@ public class BlogCommentController {
 
 	@Autowired
 	private BlogCommentService blogCommentService;
+	
+	@GetMapping("comments")
+	public ResponseEntity<List<BlogComment>> getAllComments() {
+	    List<BlogComment> comments = blogCommentService.findAll();
+	    if (!comments.isEmpty()) {
+	        return new ResponseEntity<>(comments, HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	}
+
+	@GetMapping("comments/{cid}")
+	public ResponseEntity<BlogComment> getCommentById(@PathVariable("cid") int blogCommentId) {
+	    BlogComment comment = blogCommentService.findById(blogCommentId);
+	    if (comment != null) {
+	        return new ResponseEntity<>(comment, HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	}
+
+	@GetMapping("blogs/{bid}/posts/{bpid}/comments")
+	public ResponseEntity<List<BlogComment>> getCommentsForBlogPost(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId) {
+	    List<BlogComment> comments = blogCommentService.findByBlogPost(blogPostId);
+	    if (!comments.isEmpty()) {
+	        return new ResponseEntity<>(comments, HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	}
+
+	
+    @PostMapping("blogs/{bid}/posts/{bpid}/comments")
+    public ResponseEntity<BlogComment> createComment(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId, @RequestBody BlogComment blogComment, Principal principal) {
+        BlogComment createdComment = blogCommentService.create(principal.getName(), blogPostId, blogComment);
+        if (createdComment != null) {
+            return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("blogs/{bid}/posts/{bpid}/comments/{cid}")
+    public ResponseEntity<BlogComment> updateComment(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId, @PathVariable("cid") int blogCommentId, @RequestBody BlogComment blogComment, Principal principal) {
+        BlogComment updatedComment = blogCommentService.update(principal.getName(), blogCommentId, blogComment);
+        if (updatedComment != null) {
+            return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @DeleteMapping("blogs/{bid}/posts/{bpid}/comments/{cid}")
+    public ResponseEntity<Boolean> deleteComment(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId, @PathVariable("cid") int blogCommentId, Principal principal) {
+        ResponseEntity<Boolean> response = blogCommentService.delete(principal.getName(), blogCommentId);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else if (response.getStatusCode() == HttpStatus.FORBIDDEN) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("blogs/{bid}/posts/{bpid}/comments/{cid}/replies")
+    public ResponseEntity<BlogComment> createReply(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId, @PathVariable("cid") int parentCommentId, @RequestBody BlogComment blogComment, Principal principal) {
+        BlogComment createdReply = blogCommentService.createReply(principal.getName(), parentCommentId, blogComment);
+        if (createdReply != null) {
+            return new ResponseEntity<>(createdReply, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("blogs/{bid}/posts/{bpid}/comments/{cid}/replies")
+    public ResponseEntity<List<BlogComment>> getRepliesForComment(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId, @PathVariable("cid") int parentCommentId) {
+        List<BlogComment> replies = blogCommentService.findRepliesForComment(parentCommentId);
+        if (!replies.isEmpty()) {
+            return new ResponseEntity<>(replies, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogPostController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogPostController.java
@@ -1,10 +1,22 @@
 package com.skilldistillery.howardtreasury.controllers;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.howardtreasury.entities.BlogPost;
 import com.skilldistillery.howardtreasury.services.BlogPostService;
 
 @RestController
@@ -14,4 +26,58 @@ public class BlogPostController {
 
 	@Autowired
 	private BlogPostService blogPostService;
+	
+	@GetMapping("blogs/{bid}/posts")
+    public ResponseEntity<List<BlogPost>> getAllBlogPosts(@PathVariable("bid") int blogId) {
+		List<BlogPost> blogPosts = blogPostService.findAll(blogId);
+		if(!blogPosts.isEmpty()) {
+			return new ResponseEntity<>(blogPosts, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+    }
+	
+	@GetMapping("blogs/{bid}/posts/{bpid}")
+	public ResponseEntity<BlogPost> getById(@PathVariable("bid") int blogId, @PathVariable("bpid") int blogPostId) {
+		BlogPost blogPost = blogPostService.find(blogId);
+		if(blogPost != null) {
+			return new ResponseEntity<>(blogPost, HttpStatus.OK);
+		}
+		else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PostMapping("blogs/{blogId}/posts")
+	public ResponseEntity<BlogPost> createPost(@PathVariable("blogId") int blogId, @RequestBody BlogPost blogPost, Principal principal) {
+	    BlogPost createdPost = blogPostService.create(principal.getName(), blogId, blogPost);
+	    if (createdPost != null) {
+	        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+	}
+
+	@PutMapping("blogs/{blogId}/posts/{postId}")
+	public ResponseEntity<BlogPost> updatePost(@PathVariable("blogId") int blogId, @PathVariable("postId") int postId, @RequestBody BlogPost blogPost, Principal principal) {
+	    BlogPost updatedPost = blogPostService.update(principal.getName(), postId, blogPost);
+	    if (updatedPost != null) {
+	        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	}
+
+	@DeleteMapping("blogs/{blogId}/posts/{postId}")
+	public ResponseEntity<Void> deletePost(@PathVariable("blogId") int blogId, @PathVariable("postId") int postId, Principal principal) {
+	    ResponseEntity<Void> response = blogPostService.delete(principal.getName(), postId);
+	    
+	    if (response.getStatusCode() == HttpStatus.NO_CONTENT) {
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } else if (response.getStatusCode() == HttpStatus.FORBIDDEN) {
+	        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+	    } else {
+	        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	}
+	
 }
