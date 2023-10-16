6c2ce22f134b4af34d1e061a8973602ddb77033c
Michael Harrington <mdharr7@gmail.com>
2023-10-16 13:26:01 +0800

    Generate constructor, constructor with fields, getters/setters, hashcode/equals, and toString for blog entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Blog.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Blog.java
@@ -2,6 +2,7 @@ package com.skilldistillery.howardtreasury.entities;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -29,5 +30,82 @@ public class Blog {
 	
     @OneToMany(mappedBy = "blog")
     private List<BlogPost> blogPosts;
+
+	public Blog() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Blog(int id, String title, String description, LocalDateTime createdAt, List<BlogPost> blogPosts) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.createdAt = createdAt;
+		this.blogPosts = blogPosts;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public List<BlogPost> getBlogPosts() {
+		return blogPosts;
+	}
+
+	public void setBlogPosts(List<BlogPost> blogPosts) {
+		this.blogPosts = blogPosts;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Blog other = (Blog) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Blog [id=" + id + ", title=" + title + ", description=" + description + ", createdAt=" + createdAt
+				+ "]";
+	}
 
 }
