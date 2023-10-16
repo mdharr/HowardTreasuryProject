fb041b58d9e14d838a49a7379d29a0cdcc93646f
Michael Harrington <mdharr7@gmail.com>
2023-10-16 16:50:11 +0800

    Create controller and autowire service for blog comment.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogCommentController.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.howardtreasury.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.howardtreasury.services.BlogCommentService;
+
+@RestController
+@CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
+public class BlogCommentController {
+
+	@Autowired
+	private BlogCommentService blogCommentService;
+}
