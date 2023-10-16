b8105d29fe10f7a86ac1bae91248a472d5d578f4
Michael Harrington <mdharr7@gmail.com>
2023-10-16 16:48:22 +0800

    Create controller and autowire service for blog.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/BlogController.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.howardtreasury.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.howardtreasury.services.BlogService;
+
+@RestController
+@CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
+public class BlogController {
+	
+	@Autowired
+	private BlogService blogService;
+
+}
