b7d422d8ab7b925444a094f91b5afc73a7000d30
Michael Harrington <mdharr7@gmail.com>
2023-10-16 14:56:03 +0800

    Stub out blog service methods.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/BlogService.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/BlogService.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.howardtreasury.services;
 
+import java.util.List;
+
+import com.skilldistillery.howardtreasury.entities.Blog;
+
 public interface BlogService {
 
+	public List<Blog> findAll();
+	
+	public Blog find(int blogId);
 }
