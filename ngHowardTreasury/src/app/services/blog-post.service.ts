07354cbc3a989589dec1c02399d39037223804e6
Michael Harrington <mdharr7@gmail.com>
2023-10-18 09:32:08 +0800

    Stub out index all and find methods in frontend blog post service.

--- ngHowardTreasury/src/app/services/blog-post.service.ts
+++ ngHowardTreasury/src/app/services/blog-post.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogPost } from '../models/blog-post';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostService {
+  private url = environment.baseUrl + 'api/posts';
 
-  constructor() { }
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${this.url}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('BlogPostService.indexAll(): error retrieving list of blog posts' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<BlogPost> {
+    return this.http.get<BlogPost>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('BlogPostService.find(): error retrieving blog post: ' + err)
+        );
+      })
+    );
+  }
+
 }
