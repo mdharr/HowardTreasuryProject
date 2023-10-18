6abb44c5c567816152fe7193a89eeb813af57c63
Michael Harrington <mdharr7@gmail.com>
2023-10-18 09:41:56 +0800

    Stub out blog posts component ts file.

--- ngHowardTreasury/src/app/components/blog-posts/blog-posts.component.ts
+++ ngHowardTreasury/src/app/components/blog-posts/blog-posts.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { BlogPostService } from './../../services/blog-post.service';
+import { AuthService } from './../../services/auth.service';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BlogPost } from 'src/app/models/blog-post';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-blog-posts',
   templateUrl: './blog-posts.component.html',
   styleUrls: ['./blog-posts.component.css']
 })
-export class BlogPostsComponent {
+export class BlogPostsComponent implements OnInit, OnDestroy {
+
+  // properties
+  posts: BlogPost[] = [];
+  loggedInUser: User = new User();
+
+  // subscriptions
+  private authSubscription: Subscription | undefined;
+  private blogPostSubscription: Subscription | undefined;
+
+  // service injections
+  authService = inject(AuthService);
+  blogPostService = inject(BlogPostService);
+
+  ngOnInit() {
+    this.subscribeToBlogPostIndexAll();
+  }
+
+  ngOnDestroy() {
+    this.destroyAllSubscriptions();
+  }
+
+  subscribeToBlogPostIndexAll = () => {
+    this.authSubscription =this.blogPostService.indexAll().subscribe({
+      next: (data) => {
+        this.posts = data;
+      },
+      error: (fail) => {
+        console.error('Error retrieving blog posts');
+        console.error(fail);
+
+      }
+    });
+  }
+
+  destroyAllSubscriptions = () => {
+    if(this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+    if(this.blogPostSubscription) {
+      this.blogPostSubscription.unsubscribe();
+    }
+  }
 
 }
