d108883bda3d604d5329d81889d7d29b9b7262bf
Michael Harrington <mdharr7@gmail.com>
2023-10-18 09:15:01 +0800

    Stub out blog post frontend model.

--- ngHowardTreasury/src/app/models/blog-post.ts
+++ ngHowardTreasury/src/app/models/blog-post.ts
@@ -1,2 +1,31 @@
+import { BlogComment } from "./blog-comment";
+import { User } from "./user";
+
 export class BlogPost {
+
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  user: User;
+  comments: BlogComment[];
+  hidden: boolean;
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    content: string = '',
+    createdAt: string = '',
+    user: User = new User(),
+    comments: BlogComment[] = [],
+    hidden: boolean = false
+  ) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.user = user;
+    this.comments = comments;
+    this.hidden = hidden;
+  }
 }
