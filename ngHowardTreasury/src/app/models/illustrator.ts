a367b6892f3950bed28397ad4ce03e3c06e578a6
Michael Harrington <mdharr7@gmail.com>
2023-10-27 10:58:31 +0800

    Update backend illustrator entity and frontend illustrator model to reflect newly added columns to illustrator table.

--- ngHowardTreasury/src/app/models/illustrator.ts
+++ ngHowardTreasury/src/app/models/illustrator.ts
@@ -5,17 +5,23 @@ export class Illustrator {
 
   id: number;
   name: string;
+  imageUrl: string;
+  description: string;
   collections: Collection[];
   storyImages: StoryImage[];
 
   constructor(
     id: number = 0,
     name: string = '',
+    imageUrl: string = '',
+    description: string = '',
     collections: Collection[] = [],
     storyImages: StoryImage[] = []
   ) {
     this.id = id;
     this.name = name;
+    this.imageUrl = imageUrl;
+    this.description = description;
     this.collections = collections;
     this.storyImages = storyImages;
   }
