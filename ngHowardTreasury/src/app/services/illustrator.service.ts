37026f81c7918f06607cf0aed5464ff98590c981
Michael Harrington <mdharr7@gmail.com>
2023-10-27 11:44:04 +0800

    Stub out frontend illustrator service with index and find methods in preparation for requesting illustrator data to be rendered in illustrators and illustrator-details components.

--- ngHowardTreasury/src/app/services/illustrator.service.ts
+++ ngHowardTreasury/src/app/services/illustrator.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Illustrator } from '../models/illustrator';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IllustratorService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/illustrators';
+
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<Illustrator[]> {
+    return this.http.get<Illustrator[]>(`${this.url}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('IllustratorService.indexAll(): error retrieving list of illustrators' + err)
+        );
+      })
+    );
+  }
+
+  find(id: number): Observable<Illustrator> {
+    return this.http.get<Illustrator>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('IllustratorService.find(): error retrieving illustrator: ' + err)
+        );
+      })
+    );
+  }
 }
