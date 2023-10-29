41c68247cdefd155ef7e159da19b1ba345cb7aa1
Michael Harrington <mdharr7@gmail.com>
2023-10-29 16:09:04 +0800

    Create method in backend blog post repository for retrieving blog posts by title or content if matches query parameter.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/BlogPostRepository.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/repositories/BlogPostRepository.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.howardtreasury.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.howardtreasury.entities.BlogPost;
+import com.skilldistillery.howardtreasury.entities.Story;
 
 public interface BlogPostRepository extends JpaRepository<BlogPost, Integer> {
+
+    List<BlogPost> findByTitleContainingOrContentContaining(String query);
 
 }
