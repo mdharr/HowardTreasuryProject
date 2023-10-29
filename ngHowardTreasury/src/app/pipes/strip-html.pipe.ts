f41e0ef2004837f7be4722edc751fb3219b352e9
Michael Harrington <mdharr7@gmail.com>
2023-10-29 17:00:01 +0800

    User regex expressions to replace html entities and unicode characters.

--- ngHowardTreasury/src/app/pipes/strip-html.pipe.ts
+++ ngHowardTreasury/src/app/pipes/strip-html.pipe.ts
@@ -5,9 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StripHtmlPipe implements PipeTransform {
   transform(value: string): string {
+    if (!value) {
+      return value;
+    }
     // Use a regular expression to strip HTML tags and other elements
-    const regex = /(<([^>]+)>)/gi;
-    const text = value.replace(regex, '');
+    let text = value.replace(/<[^>]*>/g, ''); // Remove HTML tags
+
+    // Use regular expressions to replace HTML entities
+    text = text.replace(/&nbsp;/g, ' ');
+    text = text.replace(/&ldquo;/g, '“');
+    text = text.replace(/&rdquo;/g, '”');
+    text = text.replace(/&rsquo;/g, '’');
+    text = text.replace(/&mdash;/g, '—');
+    text = text.replace(/&ndash;/g, '–');
+    text = text.replace(/&lsquo;/g, '‘');
+    text = text.replace(/&amp;/g, '&');
+    text = text.replace(/&hellip;/g, '…');
+    // Add more replacements as needed
 
     return text;
   }
