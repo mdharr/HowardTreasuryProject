8de3e96556f4f99441f09c85f369513225b6d628
Michael Harrington <mdharr7@gmail.com>
2023-10-29 15:53:15 +0800

    Implement recent posts component into blog post comments component. Change element type to avoid ::ng-deep bug when navigating from blog comments to home.

--- ngHowardTreasury/src/app/components/recent-posts/recent-posts.component.ts
+++ ngHowardTreasury/src/app/components/recent-posts/recent-posts.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BlogPost } from 'src/app/models/blog-post';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { BlogPostService } from 'src/app/services/blog-post.service';
 
 @Component({
   selector: 'app-recent-posts',
   templateUrl: './recent-posts.component.html',
   styleUrls: ['./recent-posts.component.css']
 })
-export class RecentPostsComponent {
+export class RecentPostsComponent implements OnInit, OnDestroy {
+  // properties
+  posts: BlogPost[] = [];
+  loggedInUser: User = new User();
 
+  // booleans
+  showAll: boolean = true;
+  showByYear: boolean = false;
+
+  // subscriptions
+  private authSubscription: Subscription | undefined;
+  private blogPostSubscription: Subscription | undefined;
+
+  // service injections
+  authService = inject(AuthService);
+  blogPostService = inject(BlogPostService);
+
+  ngOnInit() {
+    this.subscribeToBlogPostIndexAll();
+  }
+
+  ngOnDestroy() {
+    this.destroyAllSubscriptions();
+  }
+
+  subscribeToBlogPostIndexAll = () => {
+    this.blogPostSubscription = this.blogPostService.indexAll().subscribe({
+      next: (data) => {
+        this.posts = data;
+      },
+      error: (fail) => {
+        console.error('Error retrieving blog posts');
+        console.error(fail);
+
+      }
+    });
+  }
+
+  destroyAllSubscriptions = () => {
+    if(this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+    if(this.blogPostSubscription) {
+      this.blogPostSubscription.unsubscribe();
+    }
+  }
+
+  sortPosts(posts: BlogPost[]) {
+
+  }
 }
