cc035016bd92a4f6230d79b8281bc2ea9f4916b1
Michael Harrington <mdharr7@gmail.com>
2023-10-30 14:24:56 +0800

    Stub out image zoom service.

--- ngHowardTreasury/src/app/services/image-zoom.service.ts
+++ ngHowardTreasury/src/app/services/image-zoom.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageZoomService {
+  private zoomStateSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  setZoomState(zoomed: boolean) {
+    this.zoomStateSubject.next(zoomed);
+  }
+
+  getZoomState(): Observable<boolean> {
+    return this.zoomStateSubject.asObservable();
+  }
 }
