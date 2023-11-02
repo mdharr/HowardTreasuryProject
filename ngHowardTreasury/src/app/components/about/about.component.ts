cac666b98dc51f000d00326443aeb20c77bfb663
Michael Harrington <mdharr7@gmail.com>
2023-11-02 15:45:54 +0800

    Placeholder for about component img load

--- ngHowardTreasury/src/app/components/about/about.component.ts
+++ ngHowardTreasury/src/app/components/about/about.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
 
-  isLoaded: boolean = true;
+  isLoaded: boolean = false;
 
   ngOnInit() {
     window.scrollTo(0, 0);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit in AnimatedCardComponent');
+    this.checkImageLoaded();
+  }
+
+  checkImageLoaded() {
+    const image = new Image();
+    image.onload = () => {
+      this.isLoaded = true;
+    };
+    image.src = "https://goodman-games.com/wp-content/uploads/2021/01/REH-colofon.jpg";
   }
 
 }
