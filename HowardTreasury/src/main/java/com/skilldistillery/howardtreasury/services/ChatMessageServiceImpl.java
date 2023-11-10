fbaa6052a8aaa0113310ed70a8fee33ad00590a2
Michael Harrington <mdharr7@gmail.com>
2023-11-10 18:47:28 +0800

    Autowire repositories into chat related service implementations.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/ChatMessageServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/ChatMessageServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.ChatMessageRepository;
 
 @Service
 public class ChatMessageServiceImpl implements ChatMessageService {
 
+	@Autowired
+	private ChatMessageRepository chatMessageRepo;
 }
--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/UserHasChatRoomServiceImpl.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/services/UserHasChatRoomServiceImpl.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.howardtreasury.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.howardtreasury.repositories.UserHasChatRoomRepository;
 
 @Service
 public class UserHasChatRoomServiceImpl implements UserHasChatRoomService {
 
+	@Autowired
+	private UserHasChatRoomRepository userHasChatRoomRepo;
 }
