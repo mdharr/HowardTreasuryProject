8a3c78b1ba063c2a12b2c039566650bdddb4f648
Michael Harrington <mdharr7@gmail.com>
2023-11-21 13:36:32 +0800

    Add fields to weird tales entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/WeirdTales.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/WeirdTales.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.howardtreasury.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,4 +16,24 @@ public class WeirdTales {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	@Column(name = "published_at")
+	private LocalDateTime publishedAt;
+	
+	@Column(name = "page_count")
+	private int pageCount;
+	
+	@Column(name = "thumbnail_url")
+	private String thumbnailUrl;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+	
+	@Column(name = "file_url")
+	private String fileUrl;
+	
 }
