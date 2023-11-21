328050228b651d2397d45ecedc026929646c0b16
Michael Harrington <mdharr7@gmail.com>
2023-11-21 14:19:35 +0800

    Stub out REST API endpoints in weird tales rest controller. Successfully test endpoints with Postman.

--- HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/WeirdTalesController.java
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/WeirdTalesController.java
@@ -1,10 +1,17 @@
 package com.skilldistillery.howardtreasury.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.howardtreasury.entities.WeirdTales;
 import com.skilldistillery.howardtreasury.services.WeirdTalesService;
 
 @RestController
@@ -14,4 +21,28 @@ public class WeirdTalesController {
 
 	@Autowired
 	private WeirdTalesService weirdTalesService;
+	
+	@GetMapping("weird-tales")
+	public ResponseEntity<List<WeirdTales>> getAllWeirdTales() {
+		List<WeirdTales> weirdTales = weirdTalesService.findAll();
+		
+		if(weirdTales.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			return new ResponseEntity<>(weirdTales, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("weird-tales/{wtid}")
+	public ResponseEntity<WeirdTales> getWeirdTalesById(@PathVariable("wtid") int weirdTalesId) {
+		WeirdTales weirdTales = weirdTalesService.find(weirdTalesId);
+		
+		if (weirdTales != null) {
+			return new ResponseEntity<>(weirdTales, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
