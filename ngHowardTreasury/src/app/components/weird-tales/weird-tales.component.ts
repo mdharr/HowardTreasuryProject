3c581aa6c6fce927d059d89d13b691f7279c33eb
Michael Harrington <mdharr7@gmail.com>
2023-11-21 14:47:54 +0800

    Stub out weird tales component.ts file in angular frontend

--- ngHowardTreasury/src/app/components/weird-tales/weird-tales.component.ts
+++ ngHowardTreasury/src/app/components/weird-tales/weird-tales.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { WeirdTalesService } from './../../services/weird-tales.service';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { WeirdTales } from 'src/app/models/weird-tales';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-weird-tales',
   templateUrl: './weird-tales.component.html',
   styleUrls: ['./weird-tales.component.css']
 })
-export class WeirdTalesComponent {
+export class WeirdTalesComponent implements OnInit, OnDestroy {
+
+  // properties
+  weirdTales: WeirdTales[] = [];
+
+  // subscriptions declarations
+  private weirdTalesSubscription: Subscription | undefined;
+
+  // injections
+  router = inject(Router);
+  weirdTalesService = inject(WeirdTalesService);
+
+  ngOnInit() {
+    this.subscribeToWeirdTalesService();
+  }
+
+  ngOnDestroy(): void {
+    if(this.weirdTalesSubscription) {
+      this.weirdTalesSubscription.unsubscribe();
+    }
+  }
+
+  subscribeToWeirdTalesService() {
+    this.weirdTalesSubscription = this.weirdTalesService.indexAll().subscribe({
+      next: (data) => {
+        this.weirdTales = data;
+      },
+      error: (fail) => {
+        console.error('Failed to index weird tales magazine objects');
+        console.error(fail);
+      }
+    });
+  }
 
 }
