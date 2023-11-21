9645decf15197936d63134a1153cd08670b58504
Michael Harrington <mdharr7@gmail.com>
2023-11-21 14:34:04 +0800

    Stub out weird tales model in angular frontend.

--- ngHowardTreasury/src/app/models/weird-tales.ts
+++ ngHowardTreasury/src/app/models/weird-tales.ts
@@ -1,2 +1,36 @@
+import { Story } from "./story";
+
 export class WeirdTales {
+
+  id: number;
+  title: string;
+  description: string;
+  publishedAt: string;
+  pageCount: number;
+  thumbnailUrl: string;
+  imageUrl: string;
+  fileUrl: string;
+  stories: Story[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    description: string = '',
+    publishedAt: string = '',
+    pageCount: number = 0,
+    thumbnailUrl: string = '',
+    imageUrl: string = '',
+    fileUrl: string = '',
+    stories: Story[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.publishedAt = publishedAt;
+    this.pageCount = pageCount;
+    this.thumbnailUrl = thumbnailUrl;
+    this.imageUrl = imageUrl;
+    this.fileUrl = fileUrl;
+    this.stories = stories;
+  }
 }
