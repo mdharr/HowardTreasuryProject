7e407f94bfd2f0306454f28851be0f117c1e610c
Michael Harrington <mdharr7@gmail.com>
2023-11-21 13:37:49 +0800

    Generate constructors, getters/setters, hashcode/equals, and toString for weird tales entity.

--- JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/WeirdTales.java
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/WeirdTales.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.howardtreasury.entities;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -35,5 +36,111 @@ public class WeirdTales {
 	
 	@Column(name = "file_url")
 	private String fileUrl;
+
+	public WeirdTales() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WeirdTales(int id, String name, String description, LocalDateTime publishedAt, int pageCount,
+			String thumbnailUrl, String imageUrl, String fileUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.publishedAt = publishedAt;
+		this.pageCount = pageCount;
+		this.thumbnailUrl = thumbnailUrl;
+		this.imageUrl = imageUrl;
+		this.fileUrl = fileUrl;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getPublishedAt() {
+		return publishedAt;
+	}
+
+	public void setPublishedAt(LocalDateTime publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public String getThumbnailUrl() {
+		return thumbnailUrl;
+	}
+
+	public void setThumbnailUrl(String thumbnailUrl) {
+		this.thumbnailUrl = thumbnailUrl;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getFileUrl() {
+		return fileUrl;
+	}
+
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WeirdTales other = (WeirdTales) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "WeirdTales [id=" + id + ", name=" + name + ", description=" + description + ", publishedAt="
+				+ publishedAt + ", pageCount=" + pageCount + ", thumbnailUrl=" + thumbnailUrl + ", imageUrl=" + imageUrl
+				+ ", fileUrl=" + fileUrl + "]";
+	}
 	
 }
