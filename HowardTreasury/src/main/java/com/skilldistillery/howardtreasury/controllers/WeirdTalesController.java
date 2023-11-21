bb2b518bd29a469a7e6374b916936582762692d7
Michael Harrington <mdharr7@gmail.com>
2023-11-21 14:14:15 +0800

    Create REST controller for weird tales and autowire weird tales service

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/controllers/WeirdTalesController.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.howardtreasury.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.howardtreasury.services.WeirdTalesService;
+
+@RestController
+@CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
+public class WeirdTalesController {
+
+	@Autowired
+	private WeirdTalesService weirdTalesService;
+}
