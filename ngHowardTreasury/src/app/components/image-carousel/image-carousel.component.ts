9b8916a60c0c759c2aca2133402311b6b9286310
Michael Harrington <mdharr7@gmail.com>
2024-05-28 14:04:34 +0800

    Add loader to image carousel component.

--- ngHowardTreasury/src/app/components/image-carousel/image-carousel.component.ts
+++ ngHowardTreasury/src/app/components/image-carousel/image-carousel.component.ts
@@ -1,19 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.css']
 })
-export class ImageCarouselComponent {
+export class ImageCarouselComponent implements OnInit {
   @Input() images: string[] = [];
   currentImageIndex = 0;
 
+  loading: boolean = false;
+
+  ngOnInit(): void {
+    this.loadImage(this.images[this.currentImageIndex]);
+  }
+
   showNextImage() {
+    this.loading = true;
     this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    this.loadImage(this.images[this.currentImageIndex]);
   }
 
   showPreviousImage() {
+    this.loading = true;
     this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+    this.loadImage(this.images[this.currentImageIndex]);
+  }
+
+  loadImage(imageUrl: string) {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      this.loading = false;
+    };
   }
 }
