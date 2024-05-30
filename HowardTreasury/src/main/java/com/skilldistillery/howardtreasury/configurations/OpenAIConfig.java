70e01689be9e643eeb8a3bd65fde33dde5308056
Michael Harrington <mdharr7@gmail.com>
2024-05-30 14:48:25 +0800

    Create open ai config class and add spring web flux dependency to build.gradle file.

---
+++ HowardTreasury/src/main/java/com/skilldistillery/howardtreasury/configurations/OpenAIConfig.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.howardtreasury.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class OpenAIConfig {
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.openai.com/v1").build();
+    }
+}
