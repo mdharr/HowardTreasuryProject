0ac9ca67c0cdd27d4bdac3fdc144105479e3d0a0
Michael Harrington <mdharr7@gmail.com>
2024-06-25 22:44:37 +0800

    Create second method in service for working with paginated data responses from external json dummy api for testing purposes.

--- ngHowardTreasury/src/app/components/product/product.component.ts
+++ ngHowardTreasury/src/app/components/product/product.component.ts
@@ -1,29 +1,63 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
+
+interface ApiResponse {
+  products: any[];
+  total: number;
+  skip: number;
+  limit: number;
+}
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
   products: any[] = [];
+  paginatedProducts: any[] = [];
 
   productSub: Subscription | undefined;
+  paginatedProductSub: Subscription | undefined;
+  productSubsArr: Subscription[] | undefined;
 
   productService = inject(ProductService);
 
   ngOnInit(): void {
     this.subscribeToProducts();
+    this.subscribeToPaginatedProducts();
+  }
+
+  ngOnDestroy(): void {
+      if (this.productSubsArr) {
+        this.productSubsArr.forEach((sub) => {
+          sub.unsubscribe();
+        });
+      }
+      if (this.paginatedProductSub) {
+        this.paginatedProductSub.unsubscribe();
+      }
   }
 
   subscribeToProducts() {
-    this.productSub = this.productService.getAllProducts().subscribe({
-      next: (data) => {
-        this.products = data;
+    this.productSubsArr?.push(this.productService.getAllProducts().subscribe({
+      next: (data: ApiResponse) => {
+        this.products = data.products;
         console.log(this.products);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    }));
+  }
+
+  subscribeToPaginatedProducts() {
+    this.paginatedProductSub = this.productService.getPaginatedProducts().subscribe({
+      next: (data: ApiResponse) => {
+        this.paginatedProducts = data.products;
+        console.log(this.paginatedProducts);
       },
       error: (err) => {
         console.error(err);
--- ngHowardTreasury/src/app/services/product.service.ts
+++ ngHowardTreasury/src/app/services/product.service.ts
@@ -1,19 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
+interface ApiResponse {
+  products: any[];
+  total: number;
+  skip: number;
+  limit: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  page: number = 1;
+  itemsPerPage: number = 4;
+  skip: number = (this.page - 1) * this.itemsPerPage;
+
   private allProductsUrl = 'https://dummyjson.com/products';
+  private paginatedProductsUrl = `https://dummyjson.com/products?limit=${this.itemsPerPage}&skip=${this.skip}`;
 
   http = inject(HttpClient);
 
-  getAllProducts(): Observable<any[]> {
-    return this.http.get<any[]>(this.allProductsUrl).pipe(
+  getAllProducts(): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.allProductsUrl).pipe(
       catchError((err: any) => {
         console.error(err);
         return throwError(
@@ -23,4 +34,17 @@ export class ProductService {
       })
     )
   }
+
+  getPaginatedProducts(): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.paginatedProductsUrl).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+          () =>
+            new Error('ProductService.getAllProducts(): error retrieving list of products ' + err)
+        );
+      })
+    )
+  }
+
 }
