0b49d838e76304eb0a77fa4ab2e82d1629a3b329
Michael Harrington <mdharr7@gmail.com>
2024-06-25 21:45:27 +0800

    Stub out basic service for products.

--- ngHowardTreasury/src/app/services/product.service.ts
+++ ngHowardTreasury/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private allProductsUrl = environment.baseUrl + 'https://dummyjson.com/products';
+
+  http = inject(HttpClient);
+
+  getAllProducts(): Observable<any[]> {
+    return this.http.get<any[]>(this.allProductsUrl).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+          () =>
+            new Error('ProductService.getAllProducts(): error retrieving list of products ' + err)
+        );
+      })
+    )
+  }
 }
