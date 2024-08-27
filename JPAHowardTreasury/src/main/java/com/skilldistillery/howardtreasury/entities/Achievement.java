e252624b5573d28f7a5a2ece184bca01b3202195
Michael Harrington <mdharr7@gmail.com>
2024-08-27 21:07:56 +0800

    Stub out java class for Achievement entity and add to persistence xml file

---
+++ JPAHowardTreasury/src/main/java/com/skilldistillery/howardtreasury/entities/Achievement.java
@@ -0,0 +1,80 @@
+package com.skilldistillery.howardtreasury.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Achievement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String description;
+
+	public Achievement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Achievement(Long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Achievement other = (Achievement) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Achievement [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	
+
+}
