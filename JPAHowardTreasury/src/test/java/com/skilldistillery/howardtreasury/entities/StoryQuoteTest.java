f08f666a91efbf081e7dd05cbf609485753e15ad
Michael Harrington <mdharr7@gmail.com>
2024-09-06 20:55:29 +0800

    Pass first test in StoryQuote JUnit test case for entity mapping.

---
+++ JPAHowardTreasury/src/test/java/com/skilldistillery/howardtreasury/entities/StoryQuoteTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.howardtreasury.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StoryQuoteTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private StoryQuote storyQuote;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAHowardTreasury");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		storyQuote = em.find(StoryQuote.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		storyQuote = null;
+	}
+
+	@Test
+	void test_StoryQuote_entity_mapping() {
+		assertNotNull(storyQuote);
+		assertEquals("He told how murderers walk the earth Beneath the curse of Cain, "
+				+ "With crimson clouds before their eyes And flames about their brain: "
+				+ "For blood has left upon their souls Its everlasting stain.", storyQuote.getContent());
+	}
+
+}
