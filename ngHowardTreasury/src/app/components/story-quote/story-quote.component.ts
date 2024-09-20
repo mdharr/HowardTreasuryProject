012ba6aa94a6ccae725551caae0731e27b064337
Michael Harrington <mdharr7@gmail.com>
2024-09-20 19:00:17 +0800

    Generate and stub out story quote component.

--- ngHowardTreasury/src/app/components/story-quote/story-quote.component.ts
+++ ngHowardTreasury/src/app/components/story-quote/story-quote.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoryQuote } from 'src/app/models/story-quote';
 
 @Component({
   selector: 'app-story-quote',
   templateUrl: './story-quote.component.html',
   styleUrls: ['./story-quote.component.css']
 })
-export class StoryQuoteComponent {
+export class StoryQuoteComponent implements OnInit {
+
+  quote: StoryQuote = new StoryQuote();
+  quotes: StoryQuote[] = [];
+
+  ngOnInit() {
+
+  }
 
 }
