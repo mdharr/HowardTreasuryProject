959ebd244839827c1f24907b74479c1d7d8e08b9
Michael Harrington <mdharr7@gmail.com>
2024-09-20 19:12:10 +0800

    Subscribe to story quotes in story quote component. Create new util file containing fisher yates shuffle to shuffle the story quotes.

--- ngHowardTreasury/src/app/components/story-quote/story-quote.component.ts
+++ ngHowardTreasury/src/app/components/story-quote/story-quote.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Story } from './../../models/story';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { StoryQuote } from 'src/app/models/story-quote';
+import { StoryQuoteService } from 'src/app/services/story-quote.service';
 
 @Component({
   selector: 'app-story-quote',
   templateUrl: './story-quote.component.html',
   styleUrls: ['./story-quote.component.css']
 })
-export class StoryQuoteComponent implements OnInit {
+export class StoryQuoteComponent implements OnInit, OnDestroy {
 
   quote: StoryQuote = new StoryQuote();
   quotes: StoryQuote[] = [];
 
-  ngOnInit() {
+  storyQuoteSubscription: Subscription | undefined;
 
+  storyQuoteService = inject(StoryQuoteService);
+
+  ngOnInit() {
+    this.subscribeToStoryQuotes();
+  }
+
+  ngOnDestroy() {
+    if (this.storyQuoteSubscription) {
+      this.storyQuoteSubscription.unsubscribe();
+    }
+  }
+
+  subscribeToStoryQuotes() {
+    this.storyQuoteSubscription = this.storyQuoteService.indexAll().subscribe({
+      next: (data) => {
+        this.quotes = data;
+      },
+      error: (err) => {
+        console.error("Error subscribing to story quote service" + err);
+      }
+    });
   }
 
 }
